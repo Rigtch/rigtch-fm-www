b8d4fdd93450b241ba0207b99665b6f385ae5be2
Mnigos <makowskiigor@gmail.com>
2023-05-18 18:27:36 +0200

    feat: define middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import client from './apollo-client'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from './common/constants'
+
+import { REFRESH_QUERY, RefreshQuery } from '~/graphql'
+
+export async function middleware(request: NextRequest) {
+  const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value
+  const response = NextResponse.next()
+
+  if (!refreshToken) return response
+
+  const {
+    data: {
+      refresh: { accessToken },
+    },
+  } = await client.query<RefreshQuery>({
+    query: REFRESH_QUERY,
+    context: {
+      headers: {
+        Authorization: `Basic ${refreshToken}`,
+      },
+    },
+  })
+
+  response.cookies.set({
+    name: ACCESS_TOKEN,
+    value: accessToken,
+    path: '/',
+  })
+
+  return response
+}
