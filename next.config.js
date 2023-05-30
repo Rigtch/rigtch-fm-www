5f752af286b11c3dda4720e713f84c6541dfa663
Mnigos <makowskiigor@gmail.com>
2023-05-30 18:18:47 +0200

    chore: configure `stylelint`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const StylelintPlugin = require('stylelint-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: config => {
+    config.plugins.push(new StylelintPlugin())
+
+    return config
+  },
   images: {
     remotePatterns: [
       {
