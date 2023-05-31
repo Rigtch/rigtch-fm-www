17064644626b0a4386196ae8727677b042f09d18
Mnigos <makowskiigor@gmail.com>
2023-05-31 18:36:53 +0200

    feat: add favicon and metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { ApolloProvider } from '@apollo/client'
-import { AppProps } from 'next/app'
-
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
-import '~/styles/audio-bars.css'
 
+import '~/styles/audio-bars.css'
 import '~/styles/theme.css'
+
+import { ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { client } from '~/config'
 import { DefaultLayout } from '~/layouts'
@@ -15,14 +16,39 @@ import { PlaybackStateProvider } from '~/context/playback-state'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <PlaybackStateProvider>
-          <DefaultLayout>
-            <Component {...pageProps} />
-          </DefaultLayout>
-        </PlaybackStateProvider>
-      </AuthProvider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Rigtch Music</title>
+
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#0e1315" />
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" href="/aple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9400d5" />
+      </Head>
+
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <PlaybackStateProvider>
+            <DefaultLayout>
+              <Component {...pageProps} />
+            </DefaultLayout>
+          </PlaybackStateProvider>
+        </AuthProvider>
+      </ApolloProvider>
+    </>
   )
 }
