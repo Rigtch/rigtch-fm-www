01025364ce023091df55aef68b7125cdebe03ee5
Iqro-dev <arumisokolowska@gmail.com>
2023-06-06 23:21:47 +0200

    feat(top-artists): add top one artist card

--- src/components/top-artists/section.tsx
+++ src/components/top-artists/section.tsx
@@ -1,4 +1,5 @@
 import { TopArtistCard } from './card'
+import { TopOneArtist } from './top-one-artist'
 
 import { Artist } from '~/graphql/types'
 
@@ -8,10 +9,14 @@ export interface TopArtistsSectionProps {
 
 export function TopArtistsSection({ topArtists }: TopArtistsSectionProps) {
   return (
-    <section className="flex-column flex gap-3">
-      {topArtists.map((topArtist, index) => (
-        <TopArtistCard key={index} topArtist={topArtist} index={index + 1} />
-      ))}
+    <section className="flex-column align-items-center flex gap-6">
+      <TopOneArtist topArtist={topArtists[0]} />
+
+      <div className="flex-column flex w-full gap-3">
+        {topArtists.slice(1).map((topArtist, index) => (
+          <TopArtistCard key={index} topArtist={topArtist} index={index + 1} />
+        ))}
+      </div>
     </section>
   )
 }
