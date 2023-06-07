d0a115bc7c48f625f56c6fdb8e5f47bb2c3a3835
Iqro-dev <arumisokolowska@gmail.com>
2023-06-07 13:48:45 +0200

    feat(top-artists): merge two cards into one component

--- src/components/top-artists/card.tsx
+++ src/components/top-artists/card.tsx
@@ -1,5 +1,6 @@
 import { Image } from 'primereact/image'
 import { Card } from 'primereact/card'
+import { Chip } from 'primereact/chip'
 
 import { OpenInSpotifyButton } from '../common'
 
@@ -9,30 +10,48 @@ import { getImage } from '~/utils/get-image'
 
 export interface TopArtistCardProps {
   topArtist: Artist
-  index: number
+  position: number
+  topFive?: boolean
 }
 
 export function TopArtistCard({
-  topArtist: { name, href, images },
-  index,
+  topArtist: { name, href, images, genres },
+  position,
+  topFive,
 }: TopArtistCardProps) {
   return (
     <Card onClick={() => isMobile() && window.open(href, '_blank')}>
-      <main className="justify-content-between align-items-center flex flex-row gap-1">
+      <main
+        className={`justify-content-between align-items-center flex flex-row ${
+          topFive ? 'gap-4' : 'gap-1'
+        }`}
+      >
         <header className="align-items-center flex flex-row gap-4">
-          <span className="w-2rem text-center text-3xl md:text-4xl">
-            {index}
+          <span
+            className={`w-2rem text-center ${
+              topFive ? 'text-4xl md:text-5xl' : 'text-3xl md:text-4xl'
+            }`}
+          >
+            {position}
           </span>
 
           <Image
             src={getImage(images)}
             alt={name}
-            width="64"
-            height="64"
+            width={topFive ? '76' : '64'}
+            height={topFive ? '76' : '64'}
             imageClassName="border-round-md"
           />
 
-          <div className="text-xl md:text-2xl">{name}</div>
+          <div className="flex-column justify-content-center flex gap-3">
+            <div className="text-xl md:text-2xl">{name}</div>
+
+            {topFive && (
+              <div>
+                <Chip label={genres[0]} />
+              </div>
+            )}
+          </div>
         </header>
 
         <div className="flex-column justify-content-between align-items-end flex">
