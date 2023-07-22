10eef66f8874a43b21ea23ffb5e2ee026bf276f7
Mnigos <makowskiigor@gmail.com>
2023-07-22 11:18:37 +0200

    refactor(pages/about): redirect to home page if logged in

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,29 @@
+import { GetServerSideProps } from 'next'
+
+import { PageProps } from './_app'
+
+import { getProfile } from '~/api/fetchers'
+import { ACCESS_TOKEN } from '~/api/constants'
 import { ConnectCard } from '~/components/connect'
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async ({
+  req: { cookies },
+}) => {
+  try {
+    await getProfile(cookies[ACCESS_TOKEN])
+
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  } catch {
+    return {
+      props: {},
+    }
+  }
+}
 
 export default function About() {
   return (
