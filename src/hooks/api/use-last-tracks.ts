c04eadd6322f3d7f254a0b8d0f3f4ba2f6ec1ae3
Mnigos <makowskiigor@gmail.com>
2023-07-23 21:14:00 +0200

    style(hooks/api): rename add `Query` suffix

--- src/hooks/api/use-last-tracks.ts
+++ src/hooks/api/use-last-tracks.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { useCookies } from 'react-cookie'
 
-import { ACCESS_TOKEN, LAST_TRACKS } from '~/api/constants'
-import { getLastTracks } from '~/api/fetchers'
+import { ACCESS_TOKEN, LAST_TRACKS } from '@api/constants'
+import { getLastTracks } from '@api/fetchers'
 
-export const useLastTracks = () => {
+export const useLastTracksQuery = () => {
   const [cookie] = useCookies([ACCESS_TOKEN])
 
   return useQuery([LAST_TRACKS], () => getLastTracks(cookie[ACCESS_TOKEN]))
--- src/hooks/api/use-top-tracks.ts
+++ src/hooks/api/use-top-tracks.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { useCookies } from 'react-cookie'
 
-import { ACCESS_TOKEN, TOP_TRACKS } from '~/api/constants'
-import { getTopTracks } from '~/api/fetchers'
+import { ACCESS_TOKEN, TOP_TRACKS } from '@api/constants'
+import { getTopTracks } from '@api/fetchers'
 
-export const useTopTracks = () => {
+export const useTopTracksQuery = () => {
   const [cookies] = useCookies([ACCESS_TOKEN])
 
   return useQuery([TOP_TRACKS], () => getTopTracks(cookies[ACCESS_TOKEN]))
