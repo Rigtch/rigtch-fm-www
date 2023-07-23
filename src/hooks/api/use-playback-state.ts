1af9d3e6f4659de3478fc092b927f8573e285077
Mnigos <makowskiigor@gmail.com>
2023-07-23 11:20:42 +0200

    refactor(hooks/api/use-playback-state): set refetchInterval to `10_000` after rejected response

--- src/hooks/api/use-playback-state.ts
+++ src/hooks/api/use-playback-state.ts
@@ -1,17 +1,27 @@
 import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
 
 import { ACCESS_TOKEN, PLAYBACK_STATE } from '~/api/constants'
 import { getPlaybackState } from '~/api/fetchers'
 
 export const usePlaybackState = () => {
+  const [refetchInterval, setRefetchInterval] = useState(1000)
   const [cookie] = useCookies([ACCESS_TOKEN])
 
-  return useQuery(
+  const query = useQuery(
     [PLAYBACK_STATE],
     () => getPlaybackState(cookie[ACCESS_TOKEN]),
     {
-      refetchInterval: 1000,
+      refetchInterval,
     }
   )
+
+  useEffect(() => {
+    if (query.error) {
+      setRefetchInterval(10_000)
+    }
+  }, [query.error])
+
+  return query
 }
