a64feb21380c2b25fc5105e3e2b11c6faa48edb9
Iqro-riv <arumisokolowska@gmail.com>
2023-08-08 14:49:53 +0200

    feat(top-tracks): fix name overflow

--- src/sections/home/top-tracks.tsx
+++ src/sections/home/top-tracks.tsx
@@ -25,7 +25,7 @@ export function TopTracksSection() {
                 image={album.images[0].url}
                 size={ElementCardSize.LARGE}
                 key={index}
-                position={index + 1}
+                position={index + 2}
                 showFromAlbum
               />
             ))}
