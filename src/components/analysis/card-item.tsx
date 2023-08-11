7277001fbfcf6d89fc80d1a6d85ef8d22efc0f5c
Mnigos <makowskiigor@gmail.com>
2023-08-11 09:47:58 +0200

    feat(components/analysis/card): add descriptions

--- src/components/analysis/card-item.tsx
+++ src/components/analysis/card-item.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 
 export interface AnalysisCardItemProps {
   title: string
+  description: string
   value: number
   showValue?: boolean
   displayValueTemplate?: () => ReactNode
@@ -10,12 +11,13 @@ export interface AnalysisCardItemProps {
 
 export function AnalysisCardItem({
   title,
+  description,
   value,
   showValue = false,
   displayValueTemplate,
 }: AnalysisCardItemProps) {
   return (
-    <div className="col-12 md:col-4 flex flex-column gap-2">
+    <div className="col-12 md:col-4 flex flex-column gap-2 h-full">
       <header>
         <h2 className="text-xl m-0">{title}</h2>
       </header>
@@ -26,6 +28,8 @@ export function AnalysisCardItem({
           showValue={showValue}
           displayValueTemplate={displayValueTemplate}
         />
+
+        <p className="my-1 text-400">{description}</p>
       </main>
     </div>
   )
