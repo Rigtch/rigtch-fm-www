0ecb7761b2bd37adfb8368d8ce9e9b359ce00fc3
Mnigos <makowskiigor@gmail.com>
2023-08-11 09:47:58 +0200

    feat(components/analysis/card): add descriptions

--- src/components/analysis/card.tsx
+++ src/components/analysis/card.tsx
@@ -18,26 +18,56 @@ export function AnalysisCard({
   return (
     <Card>
       <div className="grid">
-        <AnalysisCardItem title="Danceability" value={danceability * 100} />
-        <AnalysisCardItem title="Acousticness" value={acousticness * 100} />
+        <AnalysisCardItem
+          title="Danceability"
+          description="Shows if your music makes you wanna dance."
+          value={danceability * 100}
+        />
+        <AnalysisCardItem
+          title="Acousticness"
+          description="Shows how acoustic is your music."
+          value={acousticness * 100}
+        />
         <AnalysisCardItem
           title="Instrumentalness"
+          description="Shows if you listen to songs without lyrics."
           value={instrumentalness * 100}
         />
       </div>
 
       <div className="grid">
-        <AnalysisCardItem title="Speechiness" value={speechiness * 100} />
-        <AnalysisCardItem title="Liveness" value={liveness * 100} />
-        <AnalysisCardItem title="Energy" value={energy * 100} />
+        <AnalysisCardItem
+          title="Speechiness"
+          description="Shows if you prefer vocals or instruments."
+          value={speechiness * 100}
+        />
+        <AnalysisCardItem
+          title="Liveness"
+          description="Shows the propability of your music being performed live."
+          value={liveness * 100}
+        />
+        <AnalysisCardItem
+          title="Energy"
+          description="Shows how energetic is your music."
+          value={energy * 100}
+        />
       </div>
 
       <div className="grid">
-        <AnalysisCardItem title="Valence" value={valence * 100} />
-        <AnalysisCardItem title="Loudness" value={loudness + 60} />
+        <AnalysisCardItem
+          title="Valence"
+          description="Shows if your music is more positive or negative."
+          value={valence * 100}
+        />
+        <AnalysisCardItem
+          title="Loudness"
+          description="Shows what level of sound you prefer."
+          value={loudness + 60}
+        />
         <AnalysisCardItem
           title="Tempo"
-          value={(tempo / 240) * 100}
+          description="The overall estimated tempo of your music in beats per minute (BPM)."
+          value={(tempo / 260) * 100}
           showValue
           displayValueTemplate={() => `${Number(tempo).toFixed(2)} BPM`}
         />
