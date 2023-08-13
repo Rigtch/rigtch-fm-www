702b0a722a5dce3a8e2ef13db36abbc774aac222
Mnigos <makowskiigor@gmail.com>
2023-08-13 11:15:20 +0200

    fix(sections): implement message if there is no items to show

--- src/sections/profile/top-tracks.tsx
+++ src/sections/profile/top-tracks.tsx
@@ -25,39 +25,43 @@ export function TopTracksSection() {
       </header>
 
       <div className="flex-column flex w-full gap-4 md:gap-8">
-        <div className="flex flex-column w-full flex-row flex-wrap gap-6">
-          <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
-            <TopOneElementCard
-              {...data[0]}
-              image={data[0]?.album.images[0].url}
-            />
+        {data.length === 0 && <p>Oops, there seems to be nothing here.</p>}
 
-            <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
-              {data.slice(1, 5).map(({ album, ...track }, index) => (
+        {data.length > 0 && (
+          <div className="flex flex-column w-full flex-row flex-wrap gap-6">
+            <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
+              <TopOneElementCard
+                {...data[0]}
+                image={data[0]?.album.images[0].url}
+              />
+
+              <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
+                {data.slice(1, 5).map(({ album, ...track }, index) => (
+                  <ElementCard
+                    {...track}
+                    album={album}
+                    image={album.images[0].url}
+                    size={ElementCardSize.LARGE}
+                    key={index}
+                    position={index + 2}
+                  />
+                ))}
+              </div>
+            </div>
+
+            <div className="flex-column flex w-full gap-2 lg:gap-3">
+              {data.slice(5).map(({ album, ...artist }, index) => (
                 <ElementCard
-                  {...track}
-                  album={album}
-                  image={album.images[0].url}
-                  size={ElementCardSize.LARGE}
                   key={index}
-                  position={index + 2}
+                  {...artist}
+                  image={album.images[0].url}
+                  size={ElementCardSize.MEDIUM}
+                  position={index + 6}
                 />
               ))}
             </div>
           </div>
-
-          <div className="flex-column flex w-full gap-2 lg:gap-3">
-            {data.slice(5).map(({ album, ...artist }, index) => (
-              <ElementCard
-                key={index}
-                {...artist}
-                image={album.images[0].url}
-                size={ElementCardSize.MEDIUM}
-                position={index + 6}
-              />
-            ))}
-          </div>
-        </div>
+        )}
       </div>
     </section>
   )
