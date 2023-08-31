47a9831fa4e51ba96c3791bae2a571e5ccd34f7e
Mnigos <makowskiigor@gmail.com>
2023-08-31 10:04:31 +0200

    feat(hooks/api): implement `infinite` query

--- src/hooks/api/use-top-artists.tsx
+++ src/hooks/api/use-top-artists.tsx
@@ -1,14 +1,34 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import { useCookies } from 'react-cookie'
 
 import { ACCESS_TOKEN, TOP_ARTISTS } from '@api/constants'
 import { getTopArtists } from '@api/fetchers'
 import { TimeRange } from '@api/types'
 
-export const useTopArtistsQuery = (timeRange = TimeRange.LONG_TERM) => {
+export const useTopArtistsQuery = (
+  timeRange = TimeRange.LONG_TERM,
+  limit = 10
+) => {
   const [cookie] = useCookies([ACCESS_TOKEN])
 
   return useQuery([TOP_ARTISTS], () =>
-    getTopArtists(cookie[ACCESS_TOKEN], timeRange)
+    getTopArtists(cookie[ACCESS_TOKEN], timeRange, limit)
+  )
+}
+
+export const useTopArtistsInfiniteQuery = (
+  timeRange = TimeRange.LONG_TERM,
+  limit = 20
+) => {
+  const [cookie] = useCookies([ACCESS_TOKEN])
+
+  return useInfiniteQuery(
+    [TOP_ARTISTS, timeRange],
+    ({ pageParam = 0 }) =>
+      getTopArtists(cookie[ACCESS_TOKEN], timeRange, limit, pageParam),
+    {
+      getNextPageParam: lastPage =>
+        lastPage.next ? lastPage.offset + limit : undefined,
+    }
   )
 }
--- src/hooks/api/use-top-tracks.tsx
+++ src/hooks/api/use-top-tracks.tsx
@@ -1,14 +1,34 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import { useCookies } from 'react-cookie'
 
 import { ACCESS_TOKEN, TOP_TRACKS } from '@api/constants'
 import { getTopTracks } from '@api/fetchers'
 import { TimeRange } from '@api/types'
 
-export const useTopTracksQuery = (timeRange = TimeRange.LONG_TERM) => {
+export const useTopTracksQuery = (
+  timeRange = TimeRange.LONG_TERM,
+  limit = 10
+) => {
   const [cookies] = useCookies([ACCESS_TOKEN])
 
   return useQuery([TOP_TRACKS], () =>
-    getTopTracks(cookies[ACCESS_TOKEN], timeRange)
+    getTopTracks(cookies[ACCESS_TOKEN], timeRange, limit)
+  )
+}
+
+export const useTopTracksInfiniteQuery = (
+  timeRange = TimeRange.LONG_TERM,
+  limit = 20
+) => {
+  const [cookies] = useCookies([ACCESS_TOKEN])
+
+  return useInfiniteQuery(
+    [TOP_TRACKS, timeRange],
+    ({ pageParam = 0 }) =>
+      getTopTracks(cookies[ACCESS_TOKEN], timeRange, limit, pageParam),
+    {
+      getNextPageParam: lastPage =>
+        lastPage.next ? lastPage.offset + limit : undefined,
+    }
   )
 }
