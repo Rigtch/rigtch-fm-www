5b1116181967945aea0961af090bdc8acd31dd93
Mnigos <makowskiigor@gmail.com>
2023-08-31 10:05:01 +0200

    test(mocks): add `spotifyResponseMockFactory`

---
+++ tests/mocks/spotify-response-factory.ts
@@ -0,0 +1,10 @@
+import { SpotifyResponse } from '@api/types'
+
+export const spotifyResponseMockFactory = <T>(
+  items: T[]
+): SpotifyResponse<T> => ({
+  href: 'https://api.spotify.com/v1/search?query=metallica&type=artist&offset=0&limit=20',
+  limit: 20,
+  next: 'https://api.spotify.com/v1/search?query=metallica&type=artist&offset=20&limit=20',
+  items,
+})
