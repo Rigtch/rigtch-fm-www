a8dd5971b4f12c5fa4d373d5d4ad6c6ab781b9cd
Mnigos <makowskiigor@gmail.com>
2023-08-31 10:06:14 +0200

    feat(component/views): implement `TopArtists` and `TopTracks`

---
+++ src/components/views/top-artists.tsx
@@ -0,0 +1,80 @@
+import { HTMLAttributes, ReactNode } from 'react'
+
+import { Artist, TimeRange } from '@api/types'
+import {
+  TopOneElementCard,
+  ElementCard,
+  ElementCardSize,
+} from '@components/element'
+import { SelectTimeRange } from '@components/select-time-range'
+
+export interface TopArtistsViewProps extends HTMLAttributes<HTMLDivElement> {
+  timeRange: TimeRange
+  setTimeRange: (timeRange: TimeRange) => void
+  items: Artist[]
+  moreItems?: ReactNode
+}
+
+export function TopArtistsView({
+  timeRange,
+  setTimeRange,
+  items,
+  moreItems,
+  children,
+}: TopArtistsViewProps) {
+  return (
+    <section className="flex-column flex w-full md:gap-2 gap-4">
+      <header className="flex flex-column md:flex-row gap-2 align-items-center justify-content-between">
+        <h2 className="text-5xl">Top Artists</h2>
+
+        <SelectTimeRange value={timeRange} onChange={setTimeRange} />
+      </header>
+
+      <main className="flex-column flex w-full gap-4 md:gap-8">
+        {items.length === 0 && <p>Oops, there seems to be nothing here.</p>}
+
+        {items.length > 0 && (
+          <>
+            <div className="flex flex-column w-full flex-row flex-wrap gap-6">
+              <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
+                <TopOneElementCard
+                  {...items[0]}
+                  image={items[0]?.images[0].url}
+                />
+
+                <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
+                  {items.slice(1, 5).map(({ images, ...artist }, index) => (
+                    <ElementCard
+                      key={index}
+                      {...artist}
+                      image={images[0].url}
+                      size={ElementCardSize.LARGE}
+                      position={index + 2}
+                      showGenres={true}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex-column flex w-full gap-2 lg:gap-3">
+              {items.slice(5).map(({ images, ...artist }, index) => (
+                <ElementCard
+                  key={index}
+                  {...artist}
+                  image={images[0].url}
+                  size={ElementCardSize.MEDIUM}
+                  position={index + 6}
+                />
+              ))}
+
+              {moreItems}
+            </div>
+          </>
+        )}
+
+        {children}
+      </main>
+    </section>
+  )
+}
---
+++ src/components/views/top-tracks.tsx
@@ -0,0 +1,78 @@
+import { HTMLAttributes, ReactNode } from 'react'
+
+import { TimeRange, Track } from '@api/types'
+import {
+  TopOneElementCard,
+  ElementCard,
+  ElementCardSize,
+} from '@components/element'
+import { SelectTimeRange } from '@components/select-time-range'
+
+export interface TopTracksViewProps extends HTMLAttributes<HTMLDivElement> {
+  timeRange: TimeRange
+  setTimeRange: (timeRange: TimeRange) => void
+  items: Track[]
+  moreItems?: ReactNode
+}
+
+export function TopTracksView({
+  timeRange,
+  setTimeRange,
+  items,
+  moreItems,
+  children,
+}: TopTracksViewProps) {
+  return (
+    <section className="flex-column flex w-full gap-4 md:gap-2">
+      <header className="flex flex-column md:flex-row gap-2 align-items-center justify-content-between">
+        <h2 className="text-5xl">Top Tracks</h2>
+
+        <SelectTimeRange value={timeRange} onChange={setTimeRange} />
+      </header>
+
+      <main className="flex-column flex w-full gap-4 md:gap-8">
+        {items.length === 0 && <p>Oops, there seems to be nothing here.</p>}
+
+        {items.length > 0 && (
+          <div className="flex flex-column w-full flex-row flex-wrap gap-6">
+            <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
+              <TopOneElementCard
+                {...items[0]}
+                image={items[0]?.album.images[0].url}
+              />
+
+              <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
+                {items.slice(1, 5).map(({ album, ...track }, index) => (
+                  <ElementCard
+                    {...track}
+                    album={album}
+                    image={album.images[0].url}
+                    size={ElementCardSize.LARGE}
+                    key={index}
+                    position={index + 2}
+                  />
+                ))}
+              </div>
+            </div>
+
+            <div className="flex-column flex w-full gap-2 lg:gap-3">
+              {items.slice(5).map(({ album, ...artist }, index) => (
+                <ElementCard
+                  key={index}
+                  {...artist}
+                  image={album.images[0].url}
+                  size={ElementCardSize.MEDIUM}
+                  position={index + 6}
+                />
+              ))}
+
+              {moreItems}
+            </div>
+          </div>
+        )}
+
+        {children}
+      </main>
+    </section>
+  )
+}
