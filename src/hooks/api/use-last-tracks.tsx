3b3b94a5215a8e72312e1b8776135bb8b2ab830d
Mnigos <makowskiigor@gmail.com>
2023-09-12 08:37:29 +0200

    feat(hooks/api): implement infinite query hook

--- src/hooks/api/use-last-tracks.tsx
+++ src/hooks/api/use-last-tracks.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import { useCookies } from 'react-cookie'
 
 import { ACCESS_TOKEN, LAST_TRACKS } from '@api/constants'
@@ -8,4 +8,19 @@ export const useLastTracksQuery = () => {
   const [cookie] = useCookies([ACCESS_TOKEN])
 
   return useQuery([LAST_TRACKS], () => getLastTracks(cookie[ACCESS_TOKEN]))
+}
+
+export const useLastTracksInfiniteQuery = (limit = 20) => {
+  const [cookie] = useCookies([ACCESS_TOKEN])
+
+  return useInfiniteQuery(
+    [LAST_TRACKS],
+    ({ pageParam }) => getLastTracks(cookie[ACCESS_TOKEN], limit, pageParam),
+    {
+      getNextPageParam: lastPage => {
+        console.log('lastPage', lastPage)
+        return lastPage.next ? lastPage.cursors.before : undefined
+      },
+    }
+  )
 }
