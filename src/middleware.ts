d51b9bad74e56355ff41628991921212e23eb920
Mnigos <makowskiigor@gmail.com>
2023-09-14 12:22:43 +0200

    fix(middleware): redirect after refreshing token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@api/constants'
-import { getRefresh } from '@api/fetchers'
+import { getProfile, getRefresh } from '@api/fetchers'
 
 export async function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value
-  const response = NextResponse.next()
+  const accessToken = request.cookies.get(ACCESS_TOKEN)?.value
 
-  if (!refreshToken) return response
+  const destinationUrl = new URL('/profile', new URL(request.url))
+  const response =
+    request.nextUrl.pathname === '/'
+      ? NextResponse.redirect(destinationUrl.toString(), {
+          status: 302,
+        })
+      : NextResponse.next()
 
-  const { accessToken } = await getRefresh(refreshToken)
+  const isLoggedIn = await getProfile(accessToken)
+    .catch(() => false)
+    .then(() => true)
+
+  if (!refreshToken || isLoggedIn) return NextResponse.next()
+
+  const { accessToken: refreshedAccesToken } = await getRefresh(refreshToken)
 
   response.cookies.set({
     name: ACCESS_TOKEN,
-    value: accessToken,
+    value: refreshedAccesToken,
     path: '/',
   })
 
