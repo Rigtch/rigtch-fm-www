762e4eb40d1cf142eec13ca618f050a545ee8c77
Mnigos <makowskiigor@gmail.com>
2023-09-17 13:09:46 +0200

    feat(components/connect/card): replace connect card with connected card if user is logged in

--- src/components/connect/card.tsx
+++ src/components/connect/card.tsx
@@ -1,26 +1,63 @@
 import { Card } from 'primereact/card'
 import Image from 'next/image'
+import { Avatar } from 'primereact/avatar'
+import { Button } from 'primereact/button'
+import { useRouter } from 'next/router'
 
 import { ConnectButton } from './button'
 
 import { spotifyLogo } from '@assets/images'
+import { useProfileQuery } from '@api/hooks'
+import { getImage } from '@utils/get-image'
 
 export function ConnectCard() {
+  const router = useRouter()
+  const { data } = useProfileQuery()
+
   return (
     <Card className="w-full">
       <div className="align-items-center flex-column flex gap-4 md:flex-row p-2">
-        <Image src={spotifyLogo} alt="Spotify's logo" width="128" />
+        {data?.displayName ? (
+          <div>
+            <Avatar
+              image={getImage(data?.images, 1) ?? spotifyLogo.src}
+              size="xlarge"
+              label={data?.displayName.slice(0, 1)}
+              shape="circle"
+              style={{ width: '128px', height: '128px' }}
+              className="border-circle"
+              pt={{
+                label: {
+                  className: 'text-6xl',
+                },
+              }}
+            />
+          </div>
+        ) : (
+          <Image src={spotifyLogo} alt="Spotify's logo" width="128" />
+        )}
 
         <div className="flex-column md:align-items-center flex gap-4 w-full">
           <div className="flex-column align-items-center flex gap-2 text-center">
-            <h1 className="m-0">With just one press of a button</h1>
+            <h1 className="m-0">
+              {data?.displayName
+                ? `Welcome ${data.displayName}!`
+                : 'With just one press of a button'}
+            </h1>
 
             <p className="m-0">
-              You&apos;ll see your top artists, favorite songs and so on.
+              With just one press of a button you&apos;ll see your top artists,
+              favorite songs and so on.
             </p>
           </div>
 
-          <ConnectButton />
+          {data?.displayName ? (
+            <Button severity="info" onClick={() => router.push('/profile')}>
+              Profile
+            </Button>
+          ) : (
+            <ConnectButton />
+          )}
         </div>
       </div>
     </Card>
