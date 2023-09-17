bbdde9a60cca547c4ebba2d8c82b38ddf84b5fbf
Iqro-riv <arumisokolowska@gmail.com>
2023-09-17 16:19:07 +0200

    fix(views): show skeleton on change time range

--- src/components/views/top-tracks.tsx
+++ src/components/views/top-tracks.tsx
@@ -33,48 +33,70 @@ export function TopTracksView({
       </header>
 
       <main className="flex-column flex w-full gap-4 md:gap-8">
-        {items.length === 0 && <p>Oops, there seems to be nothing here.</p>}
+        {/* {items.length === 0 && <p>Oops, there seems to be nothing here.</p>} */}
 
-        {items.length > 0 && (
-          <div className="flex flex-column w-full flex-row flex-wrap gap-6">
-            <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
-              <TopOneElementCard
-                skeleton={skeleton}
-                {...items[0]}
-                image={items[0]?.album.images[0].url}
-              />
+        {/* {items.length > 0 && ( */}
+        <div className="flex flex-column w-full flex-row flex-wrap gap-6">
+          <div className="justify-content-center xl:justify-content-between flex w-full flex-column md:flex-row xl:flex-nowrap gap-6">
+            <TopOneElementCard
+              skeleton={skeleton}
+              {...items[0]}
+              image={items[0]?.album.images[0].url}
+            />
 
-              <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
-                {items.slice(1, 5).map(({ album, ...track }, index) => (
-                  <ElementCard
-                    skeleton={skeleton}
-                    {...track}
-                    album={album}
-                    image={album.images[0].url}
-                    size={ElementCardSize.LARGE}
-                    key={index}
-                    position={index + 2}
-                  />
-                ))}
-              </div>
-            </div>
-
-            <div className="flex-column flex w-full gap-2 lg:gap-3">
-              {items.slice(5).map(({ album, ...artist }, index) => (
-                <ElementCard
-                  skeleton={skeleton}
-                  key={index}
-                  {...artist}
-                  image={album.images[0].url}
-                  size={ElementCardSize.MEDIUM}
-                  position={index + 6}
-                />
-              ))}
-
-              {moreItems}
+            <div className="flex-column flex gap-2 lg:gap-3 sm:w-7 w-full">
+              {skeleton
+                ? (Array.from({ length: 4 }) as Track[]).map((item, index) => (
+                    <ElementCard
+                      skeleton={skeleton}
+                      key={index}
+                      size={ElementCardSize.LARGE}
+                      position={index + 2}
+                    />
+                  ))
+                : items
+                    .slice(1, 5)
+                    .map(({ album, ...track }, index) => (
+                      <ElementCard
+                        skeleton={skeleton}
+                        {...track}
+                        album={album}
+                        image={album.images[0].url}
+                        size={ElementCardSize.LARGE}
+                        key={index}
+                        position={index + 2}
+                      />
+                    ))}
             </div>
           </div>
-        )}
+
+          <div className="flex-column flex w-full gap-2 lg:gap-3">
+            {skeleton
+              ? (Array.from({ length: 4 }) as Track[]).map((item, index) => (
+                  <ElementCard
+                    skeleton={skeleton}
+                    key={index}
+                    size={ElementCardSize.MEDIUM}
+                    position={index + 6}
+                  />
+                ))
+              : items
+                  .slice(5)
+                  .map(({ album, ...artist }, index) => (
+                    <ElementCard
+                      skeleton={skeleton}
+                      key={index}
+                      {...artist}
+                      image={album.images[0].url}
+                      size={ElementCardSize.MEDIUM}
+                      position={index + 6}
+                    />
+                  ))}
+
+            {moreItems}
+          </div>
+        </div>
+        {/* )} */}
 
         {children}
       </main>
