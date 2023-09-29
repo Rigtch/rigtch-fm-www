970b618561644fbe94237f1a560eb642fb8ade4c
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-09-29 13:48:08 +0200

    refactor(open-in-spotify-button): swap spotify icon with external link icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,2 @@
 export { default as rigtchLogo } from './rigtch-logo.png'
 export { default as spotifyIconGreen } from './spotify-icon-green.png'
-export { default as spotifyIconWhite } from './spotify-icon-white.png'
--- src/components/common/open-in-spotify-button.tsx
+++ src/components/common/open-in-spotify-button.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image'
 import Link, { LinkProps } from 'next/link'
 import { Tooltip } from 'primereact/tooltip'
-import { classNames } from 'primereact/utils'
-
-import { spotifyIconWhite } from '@assets/images'
 
 export interface OpenInSpotifyButtonProps extends Omit<LinkProps, 'href'> {
   href?: string
@@ -23,14 +19,8 @@ export function OpenInSpotifyButton({
         content="Open In Spotify"
       />
 
-      <Link
-        href={href ?? ''}
-        target="_blank"
-        className={classNames('open-in-spotify-button', className)}
-        style={{ height: 24 }}
-        {...props}
-      >
-        <Image src={spotifyIconWhite} width="24" height="24" alt={''} />
+      <Link href={href ?? ''} target="_blank" className={className} {...props}>
+        <i className="open-in-spotify-button pi pi-external-link" />
       </Link>
     </>
   )
