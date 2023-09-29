6d1489b3d77e30f5c0f478a68314f4d481c14c3e
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-09-29 14:59:10 +0200

    feat(open-in-spotify-button): change transition duration and colors

--- src/components/common/open-in-spotify-button.tsx
+++ src/components/common/open-in-spotify-button.tsx
@@ -20,7 +20,7 @@ export function OpenInSpotifyButton({
       />
 
       <Link href={href ?? ''} target="_blank" className={className} {...props}>
-        <i className="open-in-spotify-button pi pi-external-link transition-colors transition-duration-300 text-white hover:text-cyan-500 text-xl font-bold" />
+        <i className="open-in-spotify-button pi pi-external-link transition-colors transition-duration-200 text-white hover:text-cyan-500 text-xl font-bold" />
       </Link>
     </>
   )
