c37293d778b14d968c21eb34ce8b519f4c30cb58
Mnigos <makowskiigor@gmail.com>
2023-11-14 14:47:36 +0100

    fix: refreshting token

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,36 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
+import { getCookie } from 'cookies-next'
 
 import { PlaybackStateProvider } from '@context/playback-state'
+import { REFRESH_TOKEN } from '@api/constants'
+import { getRefresh } from '@api/fetchers'
 
 export interface ProvidersProps {
   children: ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  const [queryClient] = useState(() => new QueryClient())
+  const refreshToken = getCookie(REFRESH_TOKEN)
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            onError: async error => {
+              if (
+                error instanceof Error &&
+                error.message === 'The access token expired' &&
+                refreshToken
+              )
+                await getRefresh(refreshToken)
+            },
+          },
+        },
+      })
+  )
 
   return (
     <QueryClientProvider client={queryClient}>
