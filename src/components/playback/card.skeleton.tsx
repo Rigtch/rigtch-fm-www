c9f5593e7008b4ceccffea5e796d333c32af45fa
Mnigos <makowskiigor@gmail.com>
2023-11-15 16:00:44 +0100

    feat(components/playback/card): implement skeleton

---
+++ src/components/playback/card.skeleton.tsx
@@ -0,0 +1,34 @@
+import { PlaybackStateToggleButton } from './state-toggle-button'
+
+import { AudioBars } from '@components/common'
+import { Card, CardFooter, CardHeader, CardTitle } from '@components/ui/card'
+import { Skeleton } from '@components/ui/skeleton'
+
+export function PlaybackCardSkeleton() {
+  return (
+    <Card className="bg-neutral-800/50 border-neutral-800/50 p-4 w-full h-full items-center !m-0 lg:w-[380px] xl:min-w-[380px]">
+      <CardHeader className="flex flex-col sm:flex-row gap-4 p-0 w-full">
+        <Skeleton className="h-[128px] w-[128px]" />
+
+        <div className="flex flex-col justify-between gap-4 md:gap-0">
+          <CardTitle className="whitespace-nowrap w-full font-normal flex flex-col gap-3">
+            <Skeleton className="h-7 w-48" />
+            <Skeleton className="h-3 w-16" />
+          </CardTitle>
+
+          <CardFooter className="flex justify-between items-center">
+            <div className="flex gap-2 items-center">
+              <AudioBars isPlaying={false} />
+
+              <PlaybackStateToggleButton
+                isPlaying={false}
+                isDeviceAvailable={false}
+                toggleState={() => Promise.resolve()}
+              />
+            </div>
+          </CardFooter>
+        </div>
+      </CardHeader>
+    </Card>
+  )
+}
