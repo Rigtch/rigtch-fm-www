6a6b5fb707493981a14626109a34af044a10e169
Mnigos <makowskiigor@gmail.com>
2023-11-17 14:45:47 +0100

    style: move `time-range` to `common/constants`

--- src/utils/time-range.ts
+++ src/utils/time-range.ts
@@ -1,4 +1,5 @@
 import { TimeRange } from '@api/types'
+import { TIME_RANGE } from '@common/constants'
 import { SearchParams } from '@common/types'
 
 export function isTimeRange(value: string): value is TimeRange {
@@ -12,7 +13,7 @@ export function validateTimeRange(timeRange?: string | string[] | null) {
 }
 
 export function getTimeRangeFromSearchParams(searchParams: SearchParams) {
-  const timeRange = searchParams['time-range']
+  const timeRange = searchParams[TIME_RANGE]
 
   return validateTimeRange(timeRange)
 }
