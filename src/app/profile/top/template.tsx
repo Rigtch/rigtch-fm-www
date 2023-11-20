433dc5b5c444dcad6d4c8882bd37fb7ba4b64433
Mnigos <makowskiigor@gmail.com>
2023-11-20 14:15:33 +0100

    style(components/common): rename component `SelectTimeRange` -> `ToggleTimeRange`

--- src/app/profile/top/template.tsx
+++ src/app/profile/top/template.tsx
@@ -3,7 +3,7 @@
 import { useSearchParams } from 'next/navigation'
 
 import { validateTimeRange } from '@utils/time-range'
-import { SelectTimeRange } from '@components/common'
+import { ToggleTimeRange } from '@components/common'
 import { LayoutProps } from '@common/types'
 import { TIME_RANGE } from '@common/constants'
 
@@ -15,7 +15,7 @@ export default function ProfileTemplate({ children }: LayoutProps) {
   return (
     <>
       <div className="flex">
-        <SelectTimeRange initialValue={timeRange} />
+        <ToggleTimeRange initialValue={timeRange} />
       </div>
 
       {children}
