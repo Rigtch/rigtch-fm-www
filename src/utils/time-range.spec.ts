4a5698ecaca78dd269d0e05c712052722535d719
Mnigos <makowskiigor@gmail.com>
2023-11-21 14:16:47 +0100

    test(utils/time-range): implement unit tests

---
+++ src/utils/time-range.spec.ts
@@ -0,0 +1,42 @@
+import { TIME_RANGE } from './../common/constants/index'
+import { isTimeRange, validateTimeRange } from './time-range'
+
+import { SearchParams } from '@common/types'
+import { TimeRange } from '@api/types'
+
+describe('(Utils) - TimeRange', () => {
+  const timeRange = TimeRange.MEDIUM_TERM
+  const invalid = 'invalid'
+
+  describe('isTimeRange', () => {
+    it('should return true when value is a TimeRange', () => {
+      expect(isTimeRange(timeRange)).toBeTruthy()
+    })
+
+    it('should return false when value is not a TimeRange', () => {
+      expect(isTimeRange(invalid)).toBeFalsy()
+    })
+  })
+
+  describe('validateTimeRange', () => {
+    it('should return timeRange when timeRange is a TimeRange', () => {
+      expect(validateTimeRange(timeRange)).toEqual(timeRange)
+    })
+
+    it('should return TimeRange.SHORT_TERM when timeRange is not a TimeRange', () => {
+      expect(validateTimeRange(invalid)).toEqual(TimeRange.SHORT_TERM)
+    })
+  })
+
+  describe('getTimeRangeFromSearchParams', () => {
+    const searchParamsMock: SearchParams = {}
+
+    beforeEach(() => {
+      searchParamsMock[TIME_RANGE] = timeRange
+    })
+
+    it('should return timeRange when timeRange is a TimeRange', () => {
+      expect(validateTimeRange(timeRange)).toEqual(timeRange)
+    })
+  })
+})
