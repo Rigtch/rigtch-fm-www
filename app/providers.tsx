2c1970375e44a69fde752357f2e8f15fadd859f7
Mnigos <makowskiigor@gmail.com>
2023-11-24 13:11:16 +0100

    chore(deps): migrate to `react-query` v5

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,35 +1,18 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
 
 import { LayoutProps } from './types'
 
 export function RootProviders({ children }: LayoutProps) {
-  const router = useRouter()
-
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            onError: async error => {
-              if (
-                error instanceof Error &&
-                error.message === 'The access token expired'
-              ) {
-                console.log('e', error.message)
-                console.log(error.message === 'The access token expired')
-                router.refresh()
-              }
-            },
-          },
-        },
-      })
-  )
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
