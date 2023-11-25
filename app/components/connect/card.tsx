6a8a571d83a0a84cded5c8ebb556b153a6440169
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-11-25 21:07:49 +0100

    feat(home): add connect to spotify card

---
+++ app/components/connect/card.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
+
+import { ConnectButton } from './button'
+
+export function ConnectCard() {
+  return (
+    <Card className="bg-neutral-800 w-full md:w-[400px]">
+      <div className="w-full h-2 bg-neutral-700 rounded-t-xl" />
+
+      <CardHeader>
+        <CardTitle className="text-3xl">Connect to spotify</CardTitle>
+
+        <CardDescription className="text-xl">
+          With just one press of a button you&apos;ll see your top artists,
+          favorite songs and so on.
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-6 justify-center items-center pb-4">
+        <Image
+          src="/spotify-icon-green.png"
+          alt={'spotify logo'}
+          width={200}
+          height={200}
+        />
+
+        <ConnectButton />
+      </CardContent>
+    </Card>
+  )
+}
