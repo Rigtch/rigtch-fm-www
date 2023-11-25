f780f808970b898e5c430690028588c59e85c444
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-11-25 20:54:33 +0100

    feat(components/connect): remove discord card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { ConnectCard } from '@components/connect/card'
-import { DiscordCard } from '@components/connect/discord-card'
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-4 md:flex-row justify-center align-center h-full pt-6 px-4">
+    <div className="flex justify-center align-center h-full pt-6 px-4">
       <ConnectCard />
-
-      <DiscordCard />
     </div>
   )
 }
