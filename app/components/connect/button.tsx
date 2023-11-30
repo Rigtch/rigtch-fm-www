1f96329e4eeca194ff0e537e23f4cd28431cfe5f
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-11-30 15:37:15 +0100

    feat(page): finish landing page design

--- app/components/connect/button.tsx
+++ app/components/connect/button.tsx
@@ -6,9 +6,30 @@ import { Button } from '../ui/button'
 
 import { env } from '@app/config/env'
 
-export function ConnectButton() {
+export interface ConnectButtonProps {
+  className?: string
+  variant?:
+    | 'default'
+    | 'destructive'
+    | 'outline'
+    | 'secondary'
+    | 'ghost'
+    | 'link'
+    | 'success'
+    | null
+    | undefined
+}
+
+export function ConnectButton({ className, variant }: ConnectButtonProps) {
   return (
-    <Button asChild variant="success">
+    <Button
+      onClick={() =>
+        window.open(`${env.NEXT_PUBLIC_API_URL}/auth/login`, '_self')
+      }
+      asChild
+      variant={variant}
+      className={className}
+    >
       <Link href={`${env.NEXT_PUBLIC_API_URL}/auth/login`}>Connect</Link>
     </Button>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,32 @@
 import Image from 'next/image'
 
-import { HomeItemCard } from './components/common/home-card'
+import { ConnectButton } from './components/connect'
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col w-full items-center py-4 gap-[100px]">
-      <div className="flex flex-col md:flex-row gap-6 items-center justify-center w-full h-full pt-24">
+    <div className="flex flex-col w-full justify-center items-center pt-12 gap-10">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="font-semibold text-5xl">Welcome to Rigtch.fm</h1>
+
+        <h2 className="font-light text-2xl">
+          Share your music interests with your friends
+        </h2>
+
+        <ConnectButton className="px-12 py-6 text-lg" variant="default" />
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-6 items-center justify-center w-full">
         <Image
           src="/rigtch-icon.png"
           alt={'spotify logo'}
-          width={400}
-          height={400}
+          width={250}
+          height={250}
           className="h-max rounded-xl"
         />
 
-        <span className="select-none text-6xl md:text-[10vw] font-bold leading-none uppercase [-webkit-text-stroke:3px_currentcolor] [-webkit-text-fill-color:currentcolor] text-purple-500 text-opacity-70">
-          Rigtch
+        <span className="select-none text-6xl md:text-[10vw] font-bold leading-none [-webkit-text-stroke:3px_currentcolor] [-webkit-text-fill-color:currentcolor] text-purple-500 text-opacity-70">
+          rigtch.fm
         </span>
-      </div>
-
-      <div className="w-3/4">
-        <HomeItemCard />
       </div>
     </div>
   )
