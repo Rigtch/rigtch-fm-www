07d55cbc43101a6940fa2e78dfab29d0bdf31b7a
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-12-03 20:58:22 +0100

    feat(page): justify gaps and logo size

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,25 @@ import { ConnectButton } from './components/connect'
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col w-full justify-center items-center pt-12 gap-10">
-      <div className="flex flex-col justify-center items-center gap-8">
-        <h1 className="font-semibold text-5xl">Welcome to Rigtch.fm</h1>
+    <div className="flex flex-col w-full justify-center items-center pt-12 gap-24 h-[80vh]">
+      <header className="flex flex-col justify-center items-center gap-8 px-4 md:p-0">
+        <h1 className="font-semibold text-5xl text-center">
+          Welcome to Rigtch.fm
+        </h1>
 
-        <h2 className="font-light text-2xl">
+        <h2 className="font-light text-2xl text-center">
           Share your music interests with your friends
         </h2>
 
         <ConnectButton className="px-12 py-6 text-lg" variant="default" />
-      </div>
+      </header>
 
       <div className="flex flex-col md:flex-row gap-6 items-center justify-center w-full">
         <Image
           src="/rigtch-icon.png"
           alt={'spotify'}
-          width={250}
-          height={250}
+          width={150}
+          height={150}
           className="h-max rounded-xl"
         />
 
