26546cbf9582738684b50866ca77f4e2b2500b0b
Mnigos <makowskiigor@gmail.com>
2023-12-04 09:56:27 +0100

    feat(components): create footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { FaDiscord, FaGithub } from 'react-icons/fa'
+
+import { Separator } from './ui/separator'
+
+export function Footer() {
+  return (
+    <>
+      <Separator />
+
+      <footer className="p-4 flex justify-center">
+        rigtch.fm &copy; {new Date().getFullYear()} |
+        <Link
+          href="https://discord.gg/ed2J535wzc"
+          replace
+          target="_blank"
+          className="flex items-center px-2"
+        >
+          <FaDiscord />
+        </Link>
+        <Link
+          href="https://github.com/Rigtch"
+          replace
+          target="_blank"
+          className="flex items-center px-2"
+        >
+          <FaGithub />
+        </Link>
+      </footer>
+    </>
+  )
+}
