e9c243b477a2510bbee35a9ad1a53f1a7d3d1b89
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-12-05 11:33:19 +0100

    feat(page): add tests

--- app/page.spec.tsx
+++ app/page.spec.tsx
@@ -1,11 +1,41 @@
 import { render, screen } from '@testing-library/react'
+import { cookies } from 'next/headers'
 
 import HomePage from './page'
+import { ACCESS_TOKEN } from './api/constants'
+import { getProfile } from './api/fetchers'
+
+import { profileMock } from '@tests/mocks/profile'
+
+vi.mock('next/headers')
+vi.mock('next/navigation')
+vi.mock('./api/fetchers')
 
 describe('HomePage', () => {
-  test('should render with connect button', () => {
-    render(<HomePage />)
+  const accessTokenMock = 'accessToken'
 
-    expect(screen.getByText('Connect')).toBeInTheDocument()
+  beforeEach(() => {
+    vi.mocked(cookies, { partial: true }).mockReturnValue({
+      get: () => ({
+        name: ACCESS_TOKEN,
+        value: accessTokenMock,
+      }),
+    })
+  })
+
+  test('should render with rigtch.fm text', async () => {
+    render(await HomePage())
+
+    expect(screen.getByText('rigtch.fm')).toBeInTheDocument()
+  })
+
+  test('should render with profile data', async () => {
+    vi.mocked(getProfile).mockResolvedValue(profileMock)
+
+    render(await HomePage())
+
+    expect(
+      screen.getByText(`Welcome back ${profileMock.displayName}!`)
+    ).toBeInTheDocument()
   })
 })
