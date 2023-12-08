357563601c0d1b735140ca0b7ed2544eba2b6904
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-12-08 11:28:18 +0100

    feat(profile/sections): fix page tests

--- app/profile/page.spec.tsx
+++ app/profile/page.spec.tsx
@@ -1,25 +1,31 @@
 import { render, screen } from '@testing-library/react'
+import { cookies } from 'next/headers'
 
 import {
   ProfileRecentlyPlayedSection,
   ProfileTopArtistsSection,
   ProfileTopGenresSection,
   ProfileTopTracksSection,
+  ProfileAnalysisSection,
 } from './sections'
 import ProfilePage from './page'
 
 import { View } from '@app/types'
 import { TimeRange } from '@app/api/types'
 import { ToggleTimeRange } from '@app/components/common'
+import { ACCESS_TOKEN } from '@app/api/constants'
 
 vi.mock('./sections')
 vi.mock('@app/components/common')
+vi.mock('next/headers')
 
 describe('ProfilePage', () => {
+  const accessTokenMock = 'accessToken'
   const profileRecentlyPlayed = 'ProfileRecentlyPlayed'
   const profileTopArtists = 'ProfileTopArtists'
   const profileTopGenres = 'ProfileTopGenres'
   const profileTopTracks = 'ProfileTopTracks'
+  const profileAnalysis = 'ProfileAnalysis'
   const searchParams = {
     time_range: TimeRange.SHORT_TERM,
     view: View.CARD,
@@ -43,6 +49,16 @@ describe('ProfilePage', () => {
       //@ts-expect-error: Error: Objects are not valid as a React child (found: [object Promise]).
       <div>{profileRecentlyPlayed}</div>
     )
+    vi.mocked(ProfileAnalysisSection).mockReturnValue(
+      //@ts-expect-error: Error: Objects are not valid as a React child (found: [object Promise]).
+      <div>{profileAnalysis}</div>
+    )
+    vi.mocked(cookies, { partial: true }).mockReturnValue({
+      get: () => ({
+        name: ACCESS_TOKEN,
+        value: accessTokenMock,
+      }),
+    })
   })
 
   test('should render with sections', async () => {
@@ -55,6 +71,7 @@ describe('ProfilePage', () => {
     expect(screen.getByText(profileTopGenres)).toBeInTheDocument()
     expect(screen.getByText(profileTopArtists)).toBeInTheDocument()
     expect(screen.getByText(profileTopTracks)).toBeInTheDocument()
+    expect(screen.getByText(profileAnalysis)).toBeInTheDocument()
     expect(screen.getByText(profileRecentlyPlayed)).toBeInTheDocument()
   })
 
