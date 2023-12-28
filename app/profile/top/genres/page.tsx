21dcfe402d20b745366abfb47decd3340c82d54c
Iqro-dev <igor.sokolowski.code@gmail.com>
2023-12-28 13:00:47 +0100

    fix(genres/page): fix import

--- app/profile/top/genres/page.tsx
+++ app/profile/top/genres/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react'
 
-import { TopGenresLoading } from './loading'
+import TopGenresLoading from './loading'
 
 import { PageProps } from '@app/types'
 import { ProfileTopGenresSection } from '@app/profile/sections'
