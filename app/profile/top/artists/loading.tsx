cab24d2735f0e731ce360721eaacd8eda5bcce71
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-09 13:43:40 +0100

    feat(skeletons): set card as default view for artists and tracks

--- app/profile/top/artists/loading.tsx
+++ app/profile/top/artists/loading.tsx
@@ -1,6 +1,5 @@
 import TopArtistsSkeleton from '@app/profile/sections/top-artists.skeleton'
-import { SkeletonProps } from '@app/types'
 
-export default function ProfileTopArtistsLoading({ view }: SkeletonProps) {
-  return <TopArtistsSkeleton view={view} />
+export default function ProfileTopArtistsLoading() {
+  return <TopArtistsSkeleton />
 }
--- app/profile/top/tracks/loading.tsx
+++ app/profile/top/tracks/loading.tsx
@@ -1,6 +1,5 @@
 import TopTracksSkeleton from '@app/profile/sections/top-tracks.skeleton'
-import { SkeletonProps } from '@app/types'
 
-export default function ProfileTopTracksLoading({ view }: SkeletonProps) {
-  return <TopTracksSkeleton view={view} />
+export default function ProfileTopTracksLoading() {
+  return <TopTracksSkeleton />
 }
