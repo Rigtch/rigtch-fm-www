6bc5d2bf794f023eb273bc3b121a2b2addaee60f
Mnigos <makowskiigor@gmail.com>
2024-01-15 15:32:34 +0100

    refactor(middleware): fix refreshing token

--- middleware.ts
+++ middleware.ts
@@ -6,20 +6,17 @@ import {
   REFRESH_TOKEN,
 } from '@app/api/constants'
 import { getRefresh } from '@app/api/fetchers'
+import { USER_ID } from '@app/constants'
 
 export async function middleware(request: NextRequest) {
-  const response =
-    request.nextUrl.pathname === '/'
-      ? NextResponse.redirect(new URL('/profile', new URL(request.url)))
-      : NextResponse.next()
+  const userId = request.cookies.get(USER_ID)?.value
 
   const expirationDateCookie = request.cookies.get(EXPIRATION_DATE)
 
+  const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value
   const shouldRefresh =
     new Date(expirationDateCookie?.value ?? Date.now()).valueOf() - Date.now() <
     1800 * 1000
-
-  const refreshToken = request.cookies.get(REFRESH_TOKEN)?.value
 
   if (!refreshToken || !shouldRefresh) return NextResponse.next()
 
@@ -30,6 +27,10 @@ export async function middleware(request: NextRequest) {
   const expirationDate = new Date(
     Date.now() + (expiresIn ?? 0) * 1000
   ).toLocaleString()
+
+  const response = ['/', '/profile'].includes(request.nextUrl.pathname)
+    ? NextResponse.redirect(new URL(`/profile/${userId}`, new URL(request.url)))
+    : NextResponse.next()
 
   response.cookies.set({
     name: ACCESS_TOKEN,
