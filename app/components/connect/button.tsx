068ea83e402632ca66f8199ccf7c53293c60741c
Mnigos <makowskiigor@gmail.com>
2024-01-15 15:57:59 +0100

    fix(components/connect/button): redirecting to profile

--- app/components/connect/button.tsx
+++ app/components/connect/button.tsx
@@ -2,23 +2,15 @@
 
 import Link from 'next/link'
 
-import { Button } from '../ui/button'
+import { Button, ButtonProps } from '../ui/button'
 
 import { env } from '@app/config/env'
 import { Profile } from '@app/api/types'
+import { useAuthCookies } from '@app/hooks/use-auth-cookies'
 
 export interface ConnectButtonProps {
   className?: string
-  variant?:
-    | 'default'
-    | 'destructive'
-    | 'outline'
-    | 'secondary'
-    | 'ghost'
-    | 'link'
-    | 'success'
-    | null
-    | undefined
+  variant?: ButtonProps['variant']
   profile?: Profile
 }
 
@@ -27,10 +19,16 @@ export function ConnectButton({
   variant,
   profile,
 }: ConnectButtonProps) {
+  const { userId } = useAuthCookies()
+
   return (
     <Button asChild variant={variant} className={className}>
       <Link
-        href={profile ? '/profile' : `${env.NEXT_PUBLIC_API_URL}/auth/login`}
+        href={
+          profile
+            ? `/profile/${userId}`
+            : `${env.NEXT_PUBLIC_API_URL}/auth/login`
+        }
       >
         {profile ? 'Go to your profile' : 'Connect'}
       </Link>
