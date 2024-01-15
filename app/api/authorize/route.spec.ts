328571f35eb63420e8ba6ee12c322ecdc0dd55e3
Mnigos <makowskiigor@gmail.com>
2024-01-15 15:49:37 +0100

    test(api): fix tests for api routes

--- app/api/authorize/route.spec.ts
+++ app/api/authorize/route.spec.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { DeepMockProxy, mockDeep } from 'vitest-mock-extended'
+import { mockDeep } from 'vitest-mock-extended'
 
 import { GET } from './route'
 
@@ -20,28 +20,41 @@ vi.mock('next/server', () => {
 describe('/api/authorize/route', () => {
   const accessToken = 'accessToken'
   const refreshToken = 'refreshToken'
+  const id = 'id'
   const searchParams = new URLSearchParams({
     accessToken,
     refreshToken,
+    id,
   })
   const url = `http://localhost/api/authorize?${searchParams.toString()}`
-
-  let requestMock: DeepMockProxy<NextRequest>
-  let responseMock: DeepMockProxy<NextResponse>
-
-  beforeEach(() => {
-    requestMock = mockDeep<NextRequest>({
-      url,
-    })
-    responseMock = mockDeep<NextResponse>()
+  const requestMock = mockDeep<NextRequest>({
+    url,
   })
+  const responseMock = mockDeep<NextResponse>()
 
   describe('GET', () => {
     test('should save cookies and redirect to /profile', () => {
       vi.mocked(NextResponse.redirect).mockReturnValue(responseMock)
 
       expect(GET(requestMock)).toEqual(responseMock)
-      expect(responseMock.cookies.set).toHaveBeenCalledTimes(2)
+      expect(responseMock.cookies.set).toHaveBeenCalledTimes(3)
     })
+  })
+
+  test('should not save cookies if any param is missing', () => {
+    const searchParams = new URLSearchParams({
+      accessToken,
+      refreshToken,
+    })
+    const url = `http://localhost/api/authorize?${searchParams.toString()}`
+    const requestMock = mockDeep<NextRequest>({
+      url,
+    })
+    const responseMock = mockDeep<NextResponse>()
+
+    vi.mocked(NextResponse.redirect).mockReturnValue(responseMock)
+
+    expect(GET(requestMock)).toBeUndefined()
+    expect(responseMock.cookies.set).not.toHaveBeenCalled()
   })
 })
