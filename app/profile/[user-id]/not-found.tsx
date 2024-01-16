5a4d111380dddc2ddd6a4410ccaa91d424273abc
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-16 12:42:30 +0100

    feat(home): add not found page

--- app/profile/[user-id]/not-found.tsx
+++ app/profile/[user-id]/not-found.tsx
@@ -4,16 +4,26 @@ import { Button } from '@app/components/ui/button'
 
 export default function ProfileNotFound() {
   return (
-    <div>
-      <header>
-        <h2>Not Found</h2>
+    <div className="flex flex-col gap-6">
+      <header className="flex flex-col gap-4">
+        <h1 className="text-7xl">Whoops...</h1>
 
-        <p>Sorry, but profile with given uuid has not been found.</p>
+        <h2 className="text-4xl">
+          We couldn&apos;t find the profile you were looking for.
+        </h2>
+
+        <p className="text-lg">
+          This is probably because the user hasn&apos;t connected their Spotify
+          account to rigtch.fm yet. Check if the provided URL is correct and try
+          again.
+        </p>
       </header>
 
-      <Button>
-        <Link href="/profile">My Profile</Link>
-      </Button>
+      <main className="flex justify-end">
+        <Button>
+          <Link href="/profile">Go back</Link>
+        </Button>
+      </main>
     </div>
   )
 }
