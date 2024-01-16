4d6de98e36d6899a842606c40c72f1c533686fe3
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-16 13:22:27 +0100

    feat(home): implement not found page design

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,13 @@ import { Button } from '@app/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center gap-6 w-full">
-      <header className="flex flex-col items-center w-1/2 gap-6">
-        <h1 className="text-8xl">Page not found</h1>
+    <div className="relative flex flex-col overflow-hidden items-center justify-center gap-6 w-full h-[600px]">
+      <span className="text-[200px] md:text-[600px] absolute opacity-[0.1]">
+        404
+      </span>
+
+      <header className="flex flex-col items-center w-full gap-6">
+        <h1 className="text-8xl text-center">Page not found</h1>
 
         <h2 className="text-2xl">
           Are you sure you&apos;re in the right place?
