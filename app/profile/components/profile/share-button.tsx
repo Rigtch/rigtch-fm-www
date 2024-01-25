bbfbbeea3c035084579c0d52ca3398a3b77d14d5
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-25 15:31:58 +0100

    feat(share-button): make button more intuitive

--- app/profile/components/profile/share-button.tsx
+++ app/profile/components/profile/share-button.tsx
@@ -1,4 +1,4 @@
-import { CopyIcon } from '@radix-ui/react-icons'
+import { Share1Icon } from '@radix-ui/react-icons'
 
 import { Button } from '@app/components/ui/button'
 import { toast } from '@app/components/ui/use-toast'
@@ -13,8 +13,12 @@ async function saveProfileURL() {
 
 export function ShareButton() {
   return (
-    <Button variant="ghost" onClick={() => saveProfileURL()}>
-      <CopyIcon />
+    <Button
+      variant="outline"
+      className="flex hover:bg-white flex-row gap-2"
+      onClick={() => saveProfileURL()}
+    >
+      Share <Share1Icon />
     </Button>
   )
 }
