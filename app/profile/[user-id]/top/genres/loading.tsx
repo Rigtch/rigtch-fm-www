8496a4d276471a4e35f2e087b71c77edaf0615ae
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-27 16:44:23 +0100

    feat(top-genres.skeleton): improve look

--- app/profile/[user-id]/top/genres/loading.tsx
+++ app/profile/[user-id]/top/genres/loading.tsx
@@ -1,5 +1,23 @@
+import { TimeRange } from '@app/api/types'
+import { SelectView, ToggleTimeRange } from '@app/components/common'
+import { ProfileCardSkeleton } from '@app/profile/components/profile/card.skeleton'
 import { TopGenresSkeleton } from '@app/profile/sections'
+import { View } from '@app/types'
 
 export default function TopGenresLoading() {
-  return <TopGenresSkeleton />
+  return (
+    <>
+      <ProfileCardSkeleton />
+
+      <div className="flex justify-between flex-col md:flex-row gap-4 items-stretch md:items-center">
+        <ToggleTimeRange initialValue={TimeRange.SHORT_TERM} />
+
+        <div>
+          <SelectView initialValue={View.CARD} />
+        </div>
+      </div>
+
+      <TopGenresSkeleton />
+    </>
+  )
 }
