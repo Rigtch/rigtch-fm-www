355305aeac3c7366bc7c18bc9606f7aa2484e900
Mnigos <makowskiigor@gmail.com>
2024-02-07 16:04:53 +0100

    feat(app/profile): add `error` page

---
+++ app/profile/[user-id]/error.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { notFound } from 'next/navigation'
+
+import {
+  USER_NOT_FOUND,
+  VALIDATION_FAILED_UUID_EXPECTED,
+} from '@app/api/constants'
+import { ErrorProps } from '@app/types'
+
+export default function ProfileError({ error, reset }: ErrorProps) {
+  console.error(error)
+
+  if (error.message === 'The access token expired') reset()
+
+  if (
+    error instanceof Error &&
+    [VALIDATION_FAILED_UUID_EXPECTED, USER_NOT_FOUND].includes(error.message)
+  )
+    return notFound()
+}
