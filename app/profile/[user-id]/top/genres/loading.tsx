2ca60fa635abf9b29726ca573b94b6b53b5be4db
Iqro-riv <arumisokolowska@gmail.com>
2024-02-08 22:36:26 +0100

    fix(@top): util path

--- app/profile/[user-id]/top/genres/loading.tsx
+++ app/profile/[user-id]/top/genres/loading.tsx
@@ -3,11 +3,10 @@
 import { useSearchParams } from 'next/navigation'
 
 import { SelectView, ToggleTimeRange } from '@app/components/common'
-import { ProfileCardSkeleton } from '@app/profile/components/profile/card.skeleton'
 import { TopGenresSkeleton } from '@app/profile/sections'
 import { View } from '@app/types'
 import { TIME_RANGE } from '@app/constants'
-import { validateTimeRange } from '@app/utils/time-range'
+import { validateTimeRange } from '@app/profile/utils/time-range'
 
 export default function TopGenresLoading() {
   const searchParams = useSearchParams()
@@ -16,8 +15,6 @@ export default function TopGenresLoading() {
 
   return (
     <>
-      <ProfileCardSkeleton />
-
       <div className="flex justify-between flex-col md:flex-row gap-4 items-stretch md:items-center">
         <ToggleTimeRange initialValue={timeRange} />
 
