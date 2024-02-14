3faee70162dadd395b36ec223339966b50a8a44f
Mnigos <makowskiigor@gmail.com>
2024-02-14 13:00:30 +0100

    fix(app/profile/top/genres/loading): remove `SelectView` component

--- app/profile/[user-id]/top/genres/loading.tsx
+++ app/profile/[user-id]/top/genres/loading.tsx
@@ -2,9 +2,8 @@
 
 import { useSearchParams } from 'next/navigation'
 
-import { SelectView, ToggleTimeRange } from '@app/components/common'
+import { ToggleTimeRange } from '@app/components/common'
 import { TopGenresSkeleton } from '@app/profile/sections'
-import { View } from '@app/types'
 import { TIME_RANGE } from '@app/constants'
 import { validateTimeRange } from '@app/profile/utils/time-range'
 
@@ -17,10 +16,6 @@ export default function TopGenresLoading() {
     <>
       <div className="flex justify-between flex-col md:flex-row gap-4 items-stretch md:items-center">
         <ToggleTimeRange initialValue={timeRange} />
-
-        <div>
-          <SelectView initialValue={View.CARD} />
-        </div>
       </div>
 
       <TopGenresSkeleton />
