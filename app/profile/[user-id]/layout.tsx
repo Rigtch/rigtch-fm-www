724288a725978073075c47ed226e8be7c40f473f
Mnigos <makowskiigor@gmail.com>
2024-03-12 15:16:53 +0100

    fix(app/profile/layout): paraller routes, rendering children

--- app/profile/[user-id]/layout.tsx
+++ app/profile/[user-id]/layout.tsx
@@ -32,6 +32,8 @@ export default function ProfileLayout({
 
   const isProfileHomePage = pathname === `/profile/${userId}`
 
+  console.log(children)
+
   return (
     <div className="flex">
       <aside className="bg-primary border-r h-[calc(100vh-60px)] border-primary-lighter p-4 max-w-[300px] hidden lg:block top-[60px] sticky">
@@ -41,14 +43,15 @@ export default function ProfileLayout({
       <main className="my-8 md:my-16 w-full min-h-[200vh] flex flex-col items-stretch justify-start px-4 md:px-12 gap-8">
         <ProfileProviders>
           {profile}
-          {isProfileHomePage && (
+          {isProfileHomePage ? (
             <>
               {top}
               {analysis}
               {recentlyPlayed}
             </>
+          ) : (
+            <>{children}</>
           )}
-          {children}
         </ProfileProviders>
       </main>
     </div>
