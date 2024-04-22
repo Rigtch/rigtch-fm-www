8d2bedd5dce0cb3087a8a42690284c00d8bab924
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-04-22 15:45:07 +0200

    feat(items): add track page

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -2,3 +2,4 @@ export const TIME_RANGE = 'time-range' as const
 export const VIEW = 'view' as const
 export const USER_ID = 'user-id' as const
 export const ARTIST_ID = 'artist-id' as const
+export const TRACK_ID = 'track-id' as const
