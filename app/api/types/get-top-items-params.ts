59ba314e6d07793b2dd01fd4953026fea504a9b5
Mnigos <makowskiigor@gmail.com>
2024-04-23 16:02:48 +0200

    refactor(api/types): move all params to `/params`

--- app/api/types/get-top-items-params.ts
+++
@@ -1,8 +0,0 @@
-import { TimeRange } from './time-range'
-import { UsersParams } from './users-params'
-
-export interface GetTopItemsParams extends UsersParams {
-  limit?: number
-  offset?: number
-  timeRange?: TimeRange
-}
