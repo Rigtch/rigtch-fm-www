69d26c8731d9eb38d05f926f1082b8db092c8c67
Mnigos <makowskiigor@gmail.com>
2024-04-23 19:39:16 +0200

    style(profile/components/items/list): add util function `getImage`

--- app/profile/components/items/list.tsx
+++ app/profile/components/items/list.tsx
@@ -12,6 +12,9 @@ export interface ItemsListProps {
   withoutPosition?: boolean
 }
 
+const getImage = (item: Artist | Track) =>
+  'images' in item ? item.images[0].url : item.album.images[0].url
+
 export function ItemsList({ items, isTop, withoutPosition }: ItemsListProps) {
   const sortedItems = items.map((artist, index) => ({
     ...artist,
@@ -28,29 +31,13 @@ export function ItemsList({ items, isTop, withoutPosition }: ItemsListProps) {
         <div className="flex flex-col md:flex-row self-center items-center md:items-start justify-center gap-4 pt-4 mt-16 lg:mt-24 w-full">
           <div className="flex flex-col-reverse md:flex-row justify-center gap-4 md:w-2/3 h-full">
             {sortedItems.slice(0, 2).map(item => (
-              <TopItemCard
-                {...item}
-                image={
-                  'images' in item
-                    ? item.images[0].url
-                    : item.album.images[0].url
-                }
-                key={item.id}
-              />
+              <TopItemCard {...item} image={getImage(item)} key={item.id} />
             ))}
           </div>
 
           <div className="md:w-1/3 h-full">
             {sortedItems.slice(2, 3).map(item => (
-              <TopItemCard
-                {...item}
-                image={
-                  'images' in item
-                    ? item.images[0].url
-                    : item.album.images[0].url
-                }
-                key={item.id}
-              />
+              <TopItemCard {...item} image={getImage(item)} key={item.id} />
             ))}
           </div>
         </div>
@@ -59,12 +46,7 @@ export function ItemsList({ items, isTop, withoutPosition }: ItemsListProps) {
       <div className="flex flex-col gap-2">
         {sortedItems.slice(isTop ? 3 : 0).map((item, index) => (
           <div key={index}>
-            <Item
-              {...item}
-              image={
-                'images' in item ? item.images[0].url : item.album.images[0].url
-              }
-            />
+            <Item {...item} image={getImage(item)} />
 
             {index !== items.length - (isTop ? 4 : 1) && <Separator />}
           </div>
