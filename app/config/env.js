492f1b24c072be51cf4152c173cbf50f690fe8e1
Mnigos <makowskiigor@gmail.com>
2024-06-03 17:55:17 +0200

    chore(env): add spotify client and next auth variables

--- app/config/env.js
+++ app/config/env.js
@@ -5,8 +5,18 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().url(),
   },
+  server: {
+    SPOTIFY_CLIENT_ID: z.string(),
+    SPOTIFY_CLIENT_SECRET: z.string(),
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string(),
+  },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   skipValidation: process.env.NODE_ENV === 'test',
 })
