e1eaae3d76ea81ec940a3492aea1bb0baa30cc88
Mnigos <makowskiigor@gmail.com>
2024-06-03 17:58:31 +0200

    feat(providers): setup `SessionProvider`

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,25 @@
 'use client'
-
+import { SessionProvider } from 'next-auth/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
+import { Session } from 'next-auth'
 
 import { LayoutProps } from './types'
 
-export function RootProviders({ children }: LayoutProps) {
+export interface RootProvidersProps extends LayoutProps {
+  session: Session
+}
+
+export function RootProviders({ children, session }: RootProvidersProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </SessionProvider>
   )
 }
