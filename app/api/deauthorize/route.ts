3bad86c175a82288adc98c7316526e643156b240
Mnigos <makowskiigor@gmail.com>
2024-06-03 18:00:11 +0200

    refactor(api): remove `authorize` route

--- app/api/deauthorize/route.ts
+++ app/api/deauthorize/route.ts
@@ -1,14 +1,10 @@
 import { NextResponse } from 'next/server'
 
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
-
 import { USER_ID } from '@app/constants'
 
 export function GET() {
   const response = NextResponse.json({})
 
-  response.cookies.delete(ACCESS_TOKEN)
-  response.cookies.delete(REFRESH_TOKEN)
   response.cookies.delete(USER_ID)
 
   return response
