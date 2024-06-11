83c758273cd98c9e549536dc64282a9ff2a171c8
Mnigos <makowskiigor@gmail.com>
2024-06-11 22:02:45 +0200

    fix(middleware): redirect on `/profile` and `/profile/undefined`

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,18 @@
 import NextAuth from 'next-auth'
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
 import { nextAuthConfig } from '@app/auth'
+import { USER_ID } from '@app/constants'
 
-export const { auth: middleware } = NextAuth(nextAuthConfig)
+const { auth } = NextAuth(nextAuthConfig)
+
+export default auth((request: NextRequest) => {
+  if (['/profile', '/profile/undefined'].includes(request.nextUrl.pathname)) {
+    const userId = cookies().get(USER_ID)?.value
+
+    if (!userId) return NextResponse.redirect(new URL('/', request.url))
+
+    return NextResponse.redirect(new URL(`/profile/${userId}`, request.url))
+  }
+})
