566034e598880e0a3d7e05edf756fd5fa2b6a922
Mnigos <makowskiigor@gmail.com>
2024-06-12 09:47:10 +0200

    docs(components/common): document `GenreChip`

--- app/components/common/genre-chip.tsx
+++ app/components/common/genre-chip.tsx
@@ -4,7 +4,7 @@ export interface GenreChipProps {
 
 export function GenreChip({ genre }: GenreChipProps) {
   return (
-    <div className="w-max p-2 rounded-xl whitespace-nowrap bg-neutral-700">
+    <div className="w-max p-2 rounded-xl whitespace-nowrap bg-neutral-700 text-white">
       {genre}
     </div>
   )
