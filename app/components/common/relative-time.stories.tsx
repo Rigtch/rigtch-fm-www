5e36ba532531157b2a70b17b1be9225b132b4c98
Mnigos <makowskiigor@gmail.com>
2024-06-12 09:51:22 +0200

    docs(components/common): document `RelativeTime`

---
+++ app/components/common/relative-time.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { RelativeTime } from './relative-time'
+
+type RelativeTimeType = typeof RelativeTime
+
+const meta: Meta<RelativeTimeType> = {
+  title: 'Common/RelativeTime',
+  component: RelativeTime,
+  argTypes: {
+    value: {
+      control: 'text',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<RelativeTimeType>
+
+export const Default: Story = {
+  args: {
+    value: '2022-01-01',
+  },
+}
