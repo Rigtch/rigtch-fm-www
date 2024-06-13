e58cc58396b9f60fb01a02365f7fe858a81924db
Mnigos <makowskiigor@gmail.com>
2024-06-13 18:59:42 +0200

    refactor(components/items/list): move position to separate component

---
+++ app/components/items/list/element-position.tsx
@@ -0,0 +1,29 @@
+import type { HTMLAttributes } from 'react'
+
+import { cn } from '@app/utils/cn'
+
+export interface ItemsListElementPositionProps
+  extends HTMLAttributes<HTMLSpanElement> {
+  size?: 'sm' | 'md' | 'lg'
+  position: number
+}
+
+export function ItemsListElementPosition({
+  position,
+  className,
+  size = 'sm',
+}: ItemsListElementPositionProps) {
+  return (
+    <span
+      className={cn(
+        'text-center w-[2rem]',
+        size === 'sm' && 'text-sm',
+        size === 'md' && 'text-xl',
+        size === 'lg' && 'text-3xl',
+        className
+      )}
+    >
+      {position}
+    </span>
+  )
+}
