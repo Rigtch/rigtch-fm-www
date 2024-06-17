0d1e35c4c3aaf4c0891f156700e1690813ccfa4d
Mnigos <makowskiigor@gmail.com>
2024-06-17 18:49:06 +0200

    style(components/items): create examples for stories

---
+++ app/components/items/examples.ts
@@ -0,0 +1,57 @@
+export const idExample = '4530c625-2385-45d6-8db1-8b867f125e30'
+
+export const genresExample = ['Atmospheric Black Metal', 'Black Metal', 'Metal']
+
+export const trackExample = {
+  name: 'In Search for New Wisdom',
+  href: 'https://open.spotify.com/track/0QBcteLxmPLG4gAXc1pEqW',
+  album: {
+    images: [
+      {
+        height: 300,
+        width: 300,
+        url: 'https://i.scdn.co/image/ab67616d00001e02c84f17fc34c60240d9676c2f',
+      },
+    ],
+  },
+  artists: [
+    {
+      id: 'dec548ab-365f-4fe1-a2b9-c0ea52c9ff89',
+      name: 'Eldamar',
+    },
+  ],
+}
+
+export const albumExample = {
+  name: 'A Dark Forgotten Past',
+  releaseDate: '2017-12-01T00:00:00.000Z',
+  albumType: 'album',
+  href: 'https://open.spotify.com/album/7gtPnEabb5TuGYn9pGLiz8',
+  images: [
+    {
+      height: 300,
+      width: 300,
+      url: 'https://i.scdn.co/image/ab67616d00001e02c84f17fc34c60240d9676c2f',
+    },
+  ],
+  genres: genresExample,
+  artists: [
+    {
+      id: 'dec548ab-365f-4fe1-a2b9-c0ea52c9ff89',
+      name: 'Eldamar',
+    },
+  ],
+}
+
+export const artistExample = {
+  name: 'Eldamar',
+  href: 'https://open.spotify.com/artist/dec548ab-365f-4fe1-a2b9-c0ea52c9ff89',
+  images: [
+    {
+      height: 300,
+      width: 300,
+      url: 'https://i.scdn.co/image/ab67616d0000b2735fdcfafcc8e7831c5fe2c618',
+    },
+  ],
+  genres: genresExample,
+}
