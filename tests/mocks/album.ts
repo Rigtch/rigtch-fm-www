28310c3ae6881e7d71523eb7048201214300caba
Mnigos <makowskiigor@gmail.com>
2024-06-17 11:39:34 +0200

    test(mocks/album): add `href`

--- tests/mocks/album.ts
+++ tests/mocks/album.ts
@@ -2,11 +2,14 @@ import { mockDeep } from 'vitest-mock-extended'
 
 import { imagesMock } from './images'
 
+import { hrefMock } from '.'
+
 import { Album } from '@app/api/types'
 
 export const albumNameMock = 'Album 1'
 
 export const albumMock = mockDeep<Album>({
   name: albumNameMock,
+  href: hrefMock,
   images: imagesMock,
 })
