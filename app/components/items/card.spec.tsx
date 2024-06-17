e9c50428c92d6a7beb7770c3f2bbeead2336714c
Mnigos <makowskiigor@gmail.com>
2024-06-17 11:47:01 +0200

    fix(components/items/card): remove link wrapper

--- app/components/items/card.spec.tsx
+++ app/components/items/card.spec.tsx
@@ -8,43 +8,19 @@ import { trackMock } from '@tests/mocks/track'
 
 describe('ItemCard', () => {
   test('should match snapshot as album', () => {
-    const view = render(
-      <ItemCard
-        id={albumMock.id}
-        name={albumMock.name}
-        href={albumMock.href}
-        releaseDate={albumMock.releaseDate}
-        images={albumMock.images}
-        albumType={albumMock.albumType}
-      />
-    )
+    const view = render(<ItemCard {...albumMock} />)
 
     expect(view).toMatchSnapshot()
   })
 
   test('should match snapshot as track', () => {
-    const view = render(
-      <ItemCard
-        id={trackMock.id}
-        name={trackMock.name}
-        href={trackMock.href}
-        artists={trackMock.artists}
-        album={trackMock.album}
-      />
-    )
+    const view = render(<ItemCard {...trackMock} />)
 
     expect(view).toMatchSnapshot()
   })
 
   test('should match snapshot as artist', () => {
-    const view = render(
-      <ItemCard
-        id={artistMock.id}
-        name={artistMock.name}
-        href={artistMock.href}
-        images={artistMock.images}
-      />
-    )
+    const view = render(<ItemCard {...artistMock} />)
 
     expect(view).toMatchSnapshot()
   })
