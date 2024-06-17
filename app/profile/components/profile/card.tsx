0b73c16dd1643e7672427956aa6c22d9c475c00d
Mnigos <makowskiigor@gmail.com>
2024-06-17 10:13:32 +0200

    refactor: apply `FollowersCount` component

--- app/profile/components/profile/card.tsx
+++ app/profile/components/profile/card.tsx
@@ -4,7 +4,7 @@ import { ProfileAvatar } from './avatar'
 import { ShareButton } from './share-button'
 
 import { Profile } from '@app/api/types'
-import { SpotifyLink } from '@app/components/common'
+import { SpotifyLink, FollowersCount } from '@app/components/common'
 import {
   Card,
   CardDescription,
@@ -17,27 +17,27 @@ export function ProfileCard({ displayName, images, followers, href }: Profile) {
   return (
     <Card className="min-w-[75%]">
       <CardHeader className="lg:flex-row justify-between lg:items-center gap-8 p-4">
-        <div className="flex flex-wrap items-center md:justify-start justify-center gap-4 p-4 ">
+        <div className="flex flex-wrap items-center sm:justify-start justify-center gap-4 p-4 ">
           <ProfileAvatar
             src={images?.[1]?.url}
             displayName={displayName}
             size="lg"
           />
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 justify-between h-full">
             <div>
               <CardTitle className="text-2xl md:text-3xl">
                 {displayName}
               </CardTitle>
 
-              <CardDescription className="text-lg whitespace-nowrap leading-5">
-                {followers} Followers
+              <CardDescription className="text-lg whitespace-nowrap leading-5 flex items-center gap-2">
+                <SpotifyLink href={href} className="h-[20px] w-[20px]" />
+
+                <FollowersCount value={followers} />
               </CardDescription>
             </div>
 
             <div className="flex flex-row gap-2 items-left">
-              <SpotifyLink href={href} className="h-[20px] w-[20px]" />
-
               <ShareButton />
             </div>
           </div>
