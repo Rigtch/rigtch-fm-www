5be4791a123cd0297a516ee2d1d2c440e8122d40
Mnigos <makowskiigor@gmail.com>
2024-06-17 18:19:47 +0200

    test(components/items/item-top-card): create snapshot tests

---
+++ app/components/items/item-top-card.spec.tsx
@@ -0,0 +1,39 @@
+import { render } from '@testing-library/react'
+
+import { ItemTopCard } from './item-top-card'
+
+import { trackMock } from '@tests/mocks/track'
+import { albumMock } from '@tests/mocks/album'
+import { artistMock } from '@tests/mocks/artist'
+
+describe('ItemTopCard', () => {
+  test('should match snapshot as track', () => {
+    const view = render(<ItemTopCard {...trackMock} />)
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot as album', () => {
+    const view = render(<ItemTopCard {...albumMock} />)
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot as artist', () => {
+    const view = render(<ItemTopCard {...artistMock} />)
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot as top 2', () => {
+    const view = render(<ItemTopCard {...trackMock} position={2} />)
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot as top 3', () => {
+    const view = render(<ItemTopCard {...trackMock} position={3} />)
+
+    expect(view).toMatchSnapshot()
+  })
+})
