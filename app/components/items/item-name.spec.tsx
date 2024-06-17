16c22b493492a030ad83b64efdd06a7a8690f575
Mnigos <makowskiigor@gmail.com>
2024-06-17 18:19:32 +0200

    test(components/items/item-name): fix props

--- app/components/items/item-name.spec.tsx
+++ app/components/items/item-name.spec.tsx
@@ -4,14 +4,14 @@ import { ItemName } from './item-name'
 
 describe('ItemName', () => {
   test('should match snapshot', () => {
-    const view = render(<ItemName name="A Dark Forgotten Past" id="" />)
+    const view = render(<ItemName name="A Dark Forgotten Past" href="" />)
 
     expect(view).toMatchSnapshot()
   })
 
   test('should match snapshot with long name', () => {
     const view = render(
-      <ItemName name="Dark Medieval Times (Remastered 2021)" id="" />
+      <ItemName name="Dark Medieval Times (Remastered 2021)" href="" />
     )
 
     expect(view).toMatchSnapshot()
