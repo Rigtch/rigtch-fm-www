84008386b34d650c36e6f038a6bfc41732efbb9e
Mnigos <makowskiigor@gmail.com>
2024-06-19 11:15:24 +0200

    docs(components/sidebar): create stories and snapshot tests

---
+++ app/components/sidebar/sidebar.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Sidebar } from './sidebar'
+
+import { USER_ID } from '@app/constants'
+
+type SidebarType = typeof Sidebar
+type SidebarStory = StoryObj<SidebarType>
+
+export default {
+  title: 'Components/Sidebar',
+  component: Sidebar,
+  parameters: {
+    nextjs: {
+      navigation: {
+        query: {
+          example: 'value',
+        },
+        params: {
+          [USER_ID]: '1',
+        },
+        segments: [[USER_ID, '1']],
+      },
+    },
+  },
+} satisfies Meta<SidebarType>
+
+export const Default: SidebarStory = {}
