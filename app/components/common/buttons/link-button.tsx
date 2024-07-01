89cc562d8c8223bbdbe917d63a1404f2200e30d9
Mnigos <makowskiigor@gmail.com>
2024-07-01 12:15:20 +0200

    style(components/common/buttons/link-button): add import `type` specifier

--- app/components/common/buttons/link-button.tsx
+++ app/components/common/buttons/link-button.tsx
@@ -1,5 +1,6 @@
-import Link, { LinkProps } from 'next/link'
-import { ReactNode } from 'react'
+import type { LinkProps } from 'next/link'
+import Link from 'next/link'
+import type { ReactNode } from 'react'
 
 import { Button } from '../../ui/button'
 
