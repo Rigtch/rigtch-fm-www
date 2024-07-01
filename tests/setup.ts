953de99aa40556f29cecba38e22a989bdcc03056
Mnigos <makowskiigor@gmail.com>
2024-07-01 16:37:13 +0200

    test: update snapshots

--- tests/setup.ts
+++ tests/setup.ts
@@ -9,9 +9,9 @@ class MockPointerEvent extends Event {
 
   constructor(type: string, props: PointerEventInit) {
     super(type, props)
-    this.button = props.button || 0
-    this.ctrlKey = props.ctrlKey || false
-    this.pointerType = props.pointerType || 'mouse'
+    this.button = props.button ?? 0
+    this.ctrlKey = props.ctrlKey ?? false
+    this.pointerType = props.pointerType ?? 'mouse'
   }
 }
 
