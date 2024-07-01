aec8c1ff79a124bba8c86a8860706dd40248bfaa
Mnigos <makowskiigor@gmail.com>
2024-07-01 09:57:53 +0200

    feat(components/common): create `NoDataAlert`

---
+++ app/components/common/no-data-alert.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { NoDataAlert } from './no-data-alert'
+
+type NoDataAlertType = typeof NoDataAlert
+type NoDataAlertStory = StoryObj<NoDataAlertType>
+
+export default {
+  title: 'Components/Common/NoDataAlert',
+  component: NoDataAlert,
+} satisfies Meta<NoDataAlertType>
+
+export const Default: NoDataAlertStory = {}
---
+++ app/components/common/no-data-alert.tsx
@@ -0,0 +1,17 @@
+import { HiOutlineEmojiSad } from 'react-icons/hi'
+
+import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
+
+export function NoDataAlert() {
+  return (
+    <Alert variant="destructive">
+      <HiOutlineEmojiSad className="h-5 w-5" />
+
+      <AlertTitle>No Data</AlertTitle>
+
+      <AlertDescription>
+        Oops... Looks like nothing&apos;s there yet.
+      </AlertDescription>
+    </Alert>
+  )
+}
