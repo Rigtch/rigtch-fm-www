8480e92251b5d42ded9ed5a59ade2b753edb55e1
Igor Makowski <56691628+Mnigos@users.noreply.github.com>
2024-07-04 22:30:00 +0200

    Update app/components/items/examples.ts
    Co-authored-by: Iqro <arumisokolowska@gmail.com>

--- app/components/items/examples.ts
+++ app/components/items/examples.ts
@@ -114,5 +114,5 @@ export const trackExamples = [
 
 export const playsExample = 115
 export const maxPlaysExample = 1256
-export const playTimeExample = 1000 * 60 * 60 * 4 + 1000 * 60 * 10
-export const maxPlayTimeExample = 1000 * 60 * 60 * 6 + 1000 * 60 * 18
+export const playtimeExample = 1000 * 60 * 60 * 4 + 1000 * 60 * 10
+export const maxPlaytimeExample = 1000 * 60 * 60 * 6 + 1000 * 60 * 18
