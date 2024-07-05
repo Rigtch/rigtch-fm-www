2dfdf0ea806b1cbc135ef16ef4a51c32e7ab11df
Mnigos <makowskiigor@gmail.com>
2024-07-05 23:56:24 +0200

    feat(profile/utils/time-range): handle rigtch and spotify time ranges

--- app/profile/utils/time-range.ts
+++ app/profile/utils/time-range.ts
@@ -1,11 +1,30 @@
-import { TimeRange } from '@app/api/types'
+import type { StatsProvider } from '../types'
 
-export function isTimeRange(value: string): value is TimeRange {
-  return Object.values(TimeRange).includes(value as TimeRange)
+import { RigtchTimeRange, SpotifyTimeRange } from '@app/api/types'
+
+export function isSpotifyTimeRange(value: string): value is SpotifyTimeRange {
+  return Object.values(SpotifyTimeRange).includes(value as SpotifyTimeRange)
 }
 
-export function validateTimeRange(timeRange?: string | string[] | null) {
-  if (typeof timeRange === 'string' && isTimeRange(timeRange)) return timeRange
+export function isRigtchTimeRange(value: string): value is RigtchTimeRange {
+  return Object.values(RigtchTimeRange).includes(value as RigtchTimeRange)
+}
 
-  return TimeRange.SHORT_TERM
+export function validateTimeRange(
+  timeRange?: string | string[] | null,
+  provider: StatsProvider = 'spotify'
+) {
+  if (
+    provider === 'spotify' &&
+    typeof timeRange === 'string' &&
+    isSpotifyTimeRange(timeRange)
+  )
+    return timeRange
+
+  if (typeof timeRange === 'string' && isRigtchTimeRange(timeRange))
+    return timeRange
+
+  return provider === 'spotify'
+    ? SpotifyTimeRange.SHORT_TERM
+    : RigtchTimeRange.WEEK
 }
