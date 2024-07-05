844719ecf5c489e750aeeaf463e690b22a005ab6
Mnigos <makowskiigor@gmail.com>
2024-07-05 23:57:14 +0200

    feat(profile/utils/stats-provider): create validate function

---
+++ app/profile/utils/stats-provider.spec.ts
@@ -0,0 +1,28 @@
+import type { StatsProvider } from '../types'
+
+import { isStatsProvider, validateStatsProvider } from './stats-provider'
+
+describe('StatsProvider', () => {
+  const statsProvider: StatsProvider = 'spotify'
+  const invalid = 'invalid'
+
+  describe('isStatsProvider', () => {
+    test('should return true when value is a StatsProvider', () => {
+      expect(isStatsProvider(statsProvider)).toBeTruthy()
+    })
+
+    test('should return false when value is not a StatsProvider', () => {
+      expect(isStatsProvider(invalid)).toBeFalsy()
+    })
+  })
+
+  describe('validateStatsProvider', () => {
+    test('should return statsProvider when statsProvider is a StatsProvider', () => {
+      expect(validateStatsProvider(statsProvider)).toEqual(statsProvider)
+    })
+
+    test('should return StatsProvider.SPOTIFY when statsProvider is not a StatsProvider', () => {
+      expect(validateStatsProvider(invalid)).toEqual('spotify')
+    })
+  })
+})
