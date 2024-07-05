906c1328704b2be083ba2792593e702cdb531477
Mnigos <makowskiigor@gmail.com>
2024-07-05 23:51:12 +0200

    feat(api/types): add `RigtchTimeRange` and rename `TimeRange` -> `SpotifyTimeRange`

--- app/api/types/time-range.ts
+++ app/api/types/time-range.ts
@@ -1,5 +1,14 @@
-export enum TimeRange {
+export enum SpotifyTimeRange {
   SHORT_TERM = 'short_term',
   MEDIUM_TERM = 'medium_term',
   LONG_TERM = 'long_term',
+}
+
+export enum RigtchTimeRange {
+  WEEK = 'week',
+  TWO_WEEKS = 'two_weeks',
+  MONTH = 'month',
+  THREE_MONTHS = 'three_months',
+  SIX_MONTHS = 'six_months',
+  YEAR = 'year',
 }
