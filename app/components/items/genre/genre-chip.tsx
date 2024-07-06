7d5231bc445e91fa1d2cd2e530763e8879bed5af
Mnigos <makowskiigor@gmail.com>
2024-07-06 16:34:43 +0200

    feat(components/items/misc/genre/genre-chip): add tooltip with playtime or plays

--- app/components/items/genre/genre-chip.tsx
+++ app/components/items/genre/genre-chip.tsx
@@ -1,12 +1,19 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import prettyMilliseconds from 'pretty-ms'
 
 import type { PlaytimeOrPlaysProps } from '../props'
 
 import type { GenreProps } from './props'
 
 import { cn } from '@app/utils/cn'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@app/components/ui/tooltip'
 
 export type GenreChipProps = GenreProps & PlaytimeOrPlaysProps
 
@@ -31,24 +38,39 @@ export function GenreChip({
   }, [plays, playtime, maxPlays, maxPlaytime])
 
   return (
-    <div
-      className={cn(
-        'relative overflow-hidden rounded-xl w-max -z-10',
-        className ?? 'bg-neutral-700'
-      )}
-    >
-      {(plays ?? playtime) && (
-        <div
-          className="transition-all duration-700 ease-in-out -z-10 absolute bg-primary h-full -skew-x-12 -left-[8px]"
-          style={{
-            width: `calc(${progressWidth}% + 10px)`,
-          }}
-        />
-      )}
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger
+          className={cn(
+            plays ?? playtime ? 'cursor-pointer' : 'cursor-default'
+          )}
+        >
+          <div
+            className={cn(
+              'relative overflow-hidden rounded-xl w-max -z-10',
+              className ?? 'bg-neutral-700'
+            )}
+          >
+            {(plays ?? playtime) && (
+              <div
+                className="transition-all duration-700 ease-in-out -z-10 absolute bg-primary h-full -skew-x-12 -left-[8px]"
+                style={{
+                  width: `calc(${progressWidth}% + 10px)`,
+                }}
+              />
+            )}
 
-      <div className={cn('p-2 whitespace-nowrap text-white block')}>
-        {genre}
-      </div>
-    </div>
+            <div className={cn('p-2 whitespace-nowrap text-white block')}>
+              {genre}
+            </div>
+          </div>
+        </TooltipTrigger>
+
+        {plays && <TooltipContent>{plays} plays</TooltipContent>}
+        {playtime && (
+          <TooltipContent>{prettyMilliseconds(playtime)}</TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
   )
 }
