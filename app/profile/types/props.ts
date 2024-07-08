bc6eaedd0443c5df14a9f28e9513282d23584989
Mnigos <makowskiigor@gmail.com>
2024-07-08 14:29:35 +0200

    feat(profile/types/props): create `ProfilePageSearchParams` type

--- app/profile/types/props.ts
+++ app/profile/types/props.ts
@@ -1,11 +1,39 @@
-import { USER_ID } from '@app/constants'
-import { LayoutProps, PageProps } from '@app/types'
+import type { StatsProvider } from './stats-provider'
+
+import type {
+  RigtchTimeRange,
+  SpotifyTimeRange,
+  StatsMeasurement,
+} from '@app/api/types'
+import type {
+  STATS_MEASUREMENT,
+  STATS_PROVIDER,
+  TIME_RANGE,
+  USER_ID,
+  VIEW,
+} from '@app/constants'
+import type { LayoutProps, PageProps, View } from '@app/types'
 
 export interface ProfilePageProps extends PageProps {
   params: {
     [USER_ID]?: string
   }
+  searchParams: ProfilePageSearchParams
 }
+
+export type ProfilePageSearchParams =
+  | {
+      [TIME_RANGE]: SpotifyTimeRange
+      [VIEW]: View
+      [STATS_PROVIDER]: StatsProvider
+      [STATS_MEASUREMENT]?: never
+    }
+  | {
+      [VIEW]: View
+      [STATS_PROVIDER]: StatsProvider
+      [TIME_RANGE]: RigtchTimeRange
+      [STATS_MEASUREMENT]: StatsMeasurement
+    }
 
 export interface ProfileLayoutBaseProps extends LayoutProps {
   params: {
