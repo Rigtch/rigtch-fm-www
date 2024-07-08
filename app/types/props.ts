40d7317b384bac6ac9b17cdbc3412dff58263d49
Mnigos <makowskiigor@gmail.com>
2024-07-08 17:18:29 +0200

    refactor(types): remove unused types

--- app/types/props.ts
+++ app/types/props.ts
@@ -1,19 +1,7 @@
 import type { ReactNode } from 'react'
 
-import type { Params, SearchParams } from './params'
-import type { View } from './view'
-
-export interface PageProps {
-  params?: Params
-  searchParams: SearchParams
-}
-
 export interface LayoutProps {
   children: ReactNode
-}
-
-export interface SkeletonProps {
-  view?: View
 }
 
 export interface ErrorProps {
