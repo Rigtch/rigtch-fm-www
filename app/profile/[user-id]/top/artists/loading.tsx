6b89894b353191ca4a2a25f3235886184a3decf7
Mnigos <makowskiigor@gmail.com>
2024-07-08 14:48:43 +0200

    fix(profile/top/loading): remove duplicated selects

--- app/profile/[user-id]/top/artists/loading.tsx
+++ app/profile/[user-id]/top/artists/loading.tsx
@@ -3,54 +3,13 @@
 import { useSearchParams } from 'next/navigation'
 
 import { TopArtistsSkeletonSection } from '@app/profile/sections'
-import {
-  STATS_MEASUREMENT,
-  STATS_PROVIDER,
-  TIME_RANGE,
-  VIEW,
-} from '@app/constants'
-import { validateTimeRange } from '@app/profile/utils/time-range'
+import { VIEW } from '@app/constants'
 import { validateView } from '@app/profile/utils/view'
-import { ToggleStatsProvider } from '@app/profile/components/common'
-import {
-  SelectStatsMeasurement,
-  SelectTimeRange,
-  SelectView,
-} from '@app/profile/components/common/selects'
-import { validateStatsMeasurement } from '@app/profile/utils/stats-measurement'
-import { validateStatsProvider } from '@app/profile/utils/stats-provider'
-import { StatsProvider } from '@app/profile/types'
 
 export default function ProfileTopArtistsLoading() {
   const searchParams = useSearchParams()
 
-  const statsProvider = validateStatsProvider(searchParams.get(STATS_PROVIDER))
-  const statsMeasurement = validateStatsMeasurement(
-    searchParams.get(STATS_MEASUREMENT)
-  )
-  const timeRange = validateTimeRange(
-    searchParams.get(TIME_RANGE),
-    statsProvider
-  )
   const view = validateView(searchParams.get(VIEW))
 
-  return (
-    <>
-      <div className="flex justify-between flex-col md:flex-row gap-4 items-stretch md:items-center">
-        <ToggleStatsProvider initialValue={statsProvider} />
-
-        <div className="flex gap-2">
-          {statsProvider === StatsProvider.RIGTCH && (
-            <SelectStatsMeasurement initialValue={statsMeasurement} />
-          )}
-
-          <SelectTimeRange initialValue={timeRange} />
-
-          <SelectView initialValue={view} />
-        </div>
-      </div>
-
-      <TopArtistsSkeletonSection view={view} />
-    </>
-  )
+  return <TopArtistsSkeletonSection view={view} />
 }
