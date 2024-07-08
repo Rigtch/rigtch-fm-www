c65122e8219f568329915279dd48183dfb05c8a0
Mnigos <makowskiigor@gmail.com>
2024-07-08 17:06:57 +0200

    feat(constants): create `params` constants and create `ID`

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -7,3 +7,5 @@ export const USER_ID = 'user-id' as const
 export const ARTIST_ID = 'artist-id' as const
 export const TRACK_ID = 'track-id' as const
 export const ALBUM_ID = 'album-id' as const
+
+export * from './params'
