723238f1b3d64ded65115473c190f417a10ac7d4
Mnigos <makowskiigor@gmail.com>
2024-07-08 14:31:02 +0200

    feat(constants): add `STATS_PROVIDER` and `STATS_MEASUREMENT`

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,8 @@
 export const TIME_RANGE = 'time-range' as const
 export const VIEW = 'view' as const
+export const STATS_PROVIDER = 'stats-provider' as const
+export const STATS_MEASUREMENT = 'stats-measurement' as const
+
 export const USER_ID = 'user-id' as const
 export const ARTIST_ID = 'artist-id' as const
 export const TRACK_ID = 'track-id' as const
