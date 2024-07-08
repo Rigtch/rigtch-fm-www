84e8c377e2858382d87d50d42e92eb7871cd9cfc
Mnigos <makowskiigor@gmail.com>
2024-07-08 14:23:31 +0200

    refactor(profile/components/common/selects): add `SelectStatsMeasurement` and `SelectTimeRange`

---
+++ app/profile/components/common/selects/select-stats-measurement.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { LuHourglass, LuPlay } from 'react-icons/lu'
+
+import { StatsMeasurement } from '@app/api/types'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@app/components/ui/select'
+import { formatSearchParams } from '@app/profile/utils/formatters'
+import { STATS_MEASUREMENT } from '@app/constants'
+
+export interface SelectStatsMeasurementProps {
+  initialValue: StatsMeasurement
+}
+
+export function SelectStatsMeasurement({
+  initialValue,
+}: SelectStatsMeasurementProps) {
+  const statsMeasurementOptions = [
+    {
+      value: StatsMeasurement.PLAYS,
+      label: 'Plays',
+      icon: <LuPlay />,
+    },
+    {
+      value: StatsMeasurement.PLAY_TIME,
+      label: 'Playtime',
+      icon: <LuHourglass />,
+    },
+  ]
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  function handleOnValueChange(value: StatsMeasurement) {
+    router.push(
+      `${pathname}?${formatSearchParams(searchParams, STATS_MEASUREMENT, value)}`,
+      {
+        scroll: true,
+      }
+    )
+  }
+
+  return (
+    <Select defaultValue={initialValue} onValueChange={handleOnValueChange}>
+      <SelectTrigger className="min-w-[120px] text-white">
+        <SelectValue placeholder="Select stats measurement" />
+      </SelectTrigger>
+
+      <SelectContent>
+        {statsMeasurementOptions.map(({ value, label, icon }) => (
+          <SelectItem key={value} value={value} className="flex gap-2">
+            <div className="flex gap-2 items-center">
+              {icon}
+              {label}
+            </div>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
---
+++ app/profile/components/common/selects/select-time-range.spec.tsx
@@ -0,0 +1,110 @@
+import userEvent, { type UserEvent } from '@testing-library/user-event'
+import { MemoryRouterProvider } from 'next-router-mock/dist/MemoryRouterProvider'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import type { MockInstance } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import { SelectTimeRange } from './select-time-range'
+
+import { RigtchTimeRange, SpotifyTimeRange } from '@app/api/types'
+
+vi.mock('next/navigation')
+
+describe('SelectTimeRange', () => {
+  let user: UserEvent
+  let useSearchParamsSpy: MockInstance
+  let useRouterSpy: MockInstance
+  let usePathnameSpy: MockInstance
+
+  beforeEach(() => {
+    user = userEvent.setup()
+    useSearchParamsSpy = vi.mocked(useSearchParams)
+    useRouterSpy = vi.mocked(useRouter)
+    usePathnameSpy = vi.mocked(usePathname)
+  })
+
+  test('should match snapshot with spotify provider', () => {
+    const view = render(
+      <SelectTimeRange initialValue={SpotifyTimeRange.SHORT_TERM} />,
+      {
+        wrapper: MemoryRouterProvider,
+      }
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot with rigtch provider', () => {
+    const view = render(
+      <SelectTimeRange initialValue={RigtchTimeRange.WEEK} />,
+      {
+        wrapper: MemoryRouterProvider,
+      }
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should change time range with spotify provider', async () => {
+    const pushSpy = vi.fn()
+    const searchParams = new URLSearchParams()
+
+    useSearchParamsSpy.mockReturnValue(searchParams)
+    useRouterSpy.mockReturnValue({
+      push: pushSpy,
+    })
+    usePathnameSpy.mockReturnValue('/profile/id')
+
+    render(<SelectTimeRange initialValue={SpotifyTimeRange.SHORT_TERM} />, {
+      wrapper: MemoryRouterProvider,
+    })
+
+    const selectButton = screen.getByRole('combobox')
+
+    expect(selectButton).toHaveTextContent('4 weeks')
+
+    await user.click(selectButton)
+    await user.click(screen.getByRole('option', { name: '6 months' }))
+
+    expect(selectButton).toHaveTextContent('6 months')
+
+    await user.click(selectButton)
+    await user.click(screen.getByRole('option', { name: 'lifetime' }))
+
+    expect(selectButton).toHaveTextContent('lifetime')
+  })
+
+  test('should change time range with rigtch provider', async () => {
+    const pushSpy = vi.fn()
+    const searchParams = new URLSearchParams()
+
+    useSearchParamsSpy.mockReturnValue(searchParams)
+    useRouterSpy.mockReturnValue({
+      push: pushSpy,
+    })
+    usePathnameSpy.mockReturnValue('/profile/id')
+
+    render(<SelectTimeRange initialValue={RigtchTimeRange.WEEK} />, {
+      wrapper: MemoryRouterProvider,
+    })
+
+    const selectButton = screen.getByRole('combobox')
+
+    expect(selectButton).toHaveTextContent('7 days')
+
+    await user.click(selectButton)
+    await user.click(screen.getByRole('option', { name: '14 days' }))
+
+    expect(selectButton).toHaveTextContent('14 days')
+
+    await user.click(selectButton)
+    await user.click(screen.getByRole('option', { name: '30 days' }))
+
+    expect(selectButton).toHaveTextContent('30 days')
+
+    await user.click(selectButton)
+    await user.click(screen.getByRole('option', { name: '90 days' }))
+
+    expect(selectButton).toHaveTextContent('90 days')
+  })
+})
