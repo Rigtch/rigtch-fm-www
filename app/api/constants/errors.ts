c5e103b0e3b1d5ba5a9f84338a3c794bbf590842
Mnigos <makowskiigor@gmail.com>
2024-07-09 12:58:16 +0200

    refactor(api/constants/errors): add const assertion

--- app/api/constants/errors.ts
+++ app/api/constants/errors.ts
@@ -1,4 +1,4 @@
 export const VALIDATION_FAILED_UUID_EXPECTED =
-  'Validation failed (uuid is expected)'
+  'Validation failed (uuid is expected)' as const
 
-export const USER_NOT_FOUND = 'The user has not been found.'
+export const USER_NOT_FOUND = 'The user has not been found.' as const
