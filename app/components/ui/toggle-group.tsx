933652b54aca8a3e8b113a18aaca65567f3859c1
Mnigos <makowskiigor@gmail.com>
2024-07-23 16:17:37 +0200

    fix(components/ui/toggle-group): disabled should be more visible

--- app/components/ui/toggle-group.tsx
+++ app/components/ui/toggle-group.tsx
@@ -19,10 +19,15 @@ const ToggleGroup = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
     VariantProps<typeof toggleVariants>
->(({ className, variant, size, children, ...props }, ref) => (
+>(({ className, variant, size, children, disabled, ...props }, ref) => (
   <ToggleGroupPrimitive.Root
     ref={ref}
-    className={cn('flex items-center justify-center gap-1', className)}
+    className={cn(
+      'flex items-center justify-center gap-1',
+      disabled && 'opacity-50',
+      className
+    )}
+    disabled={disabled}
     {...props}
   >
     <ToggleGroupContext.Provider value={{ variant, size }}>
@@ -37,7 +42,7 @@ const ToggleGroupItem = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
     VariantProps<typeof toggleVariants>
->(({ className, children, variant, size, ...props }, ref) => {
+>(({ className, children, variant, size, disabled, ...props }, ref) => {
   const context = React.useContext(ToggleGroupContext)
 
   return (
@@ -48,8 +53,12 @@ const ToggleGroupItem = React.forwardRef<
           variant: context.variant ?? variant,
           size: context.size ?? size,
         }),
+        disabled
+          ? 'cursor-not-allowed hover:bg-transparent hover:text-white'
+          : 'cursor-pointer',
         className
       )}
+      disabled={disabled}
       {...props}
     >
       {children}
