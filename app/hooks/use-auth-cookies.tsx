da6c571d70b91c768b189bdd18d09571e07b2c73
Mnigos <makowskiigor@gmail.com>
2024-07-24 14:11:15 +0200

    refactor(hooks/use-auth-cookies): remove unused `removeAuthCookies` method

--- app/hooks/use-auth-cookies.tsx
+++ app/hooks/use-auth-cookies.tsx
@@ -3,14 +3,9 @@ import { useCookies } from 'react-cookie'
 import { USER_ID } from '@app/constants'
 
 export const useAuthCookies = () => {
-  const [cookie, , removeCookies] = useCookies([USER_ID])
+  const [cookie] = useCookies([USER_ID])
 
   return {
     userId: cookie[USER_ID] as string,
-    removeAuthCookies: async () => {
-      removeCookies(USER_ID)
-
-      await fetch('/api/deauthorize')
-    },
   }
 }
