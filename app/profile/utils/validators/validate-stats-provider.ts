d6712672094113d1a836e5756b8ed86a83bb2d94
Mnigos <makowskiigor@gmail.com>
2024-07-24 11:00:58 +0200

    feat(profile/utils/validators/stats-provider): set default stats provider to rigtch
    If user exists for more than 7 days then the default stats provider will be rigtch.fm

--- app/profile/utils/validators/validate-stats-provider.ts
+++ app/profile/utils/validators/validate-stats-provider.ts
@@ -1,14 +1,19 @@
-import { StatsProvider } from '@app/profile/enums'
+import { isTimeRangeDisabled } from '@app/profile/utils/helpers'
+import { RigtchTimeRange, StatsProvider } from '@app/profile/enums'
 
 function isStatsProvider(value: string): value is StatsProvider {
   return Object.values(StatsProvider).includes(value as StatsProvider)
 }
 
 export function validateStatsProvider(
-  statsProvider?: string | string[] | null
+  statsProvider?: string | string[] | null,
+  userCreatedAt?: Date
 ) {
   if (typeof statsProvider === 'string' && isStatsProvider(statsProvider))
     return statsProvider
 
-  return StatsProvider.SPOTIFY
+  return userCreatedAt &&
+    isTimeRangeDisabled(RigtchTimeRange.WEEK, userCreatedAt)
+    ? StatsProvider.SPOTIFY
+    : StatsProvider.RIGTCH
 }
