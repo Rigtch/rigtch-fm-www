94c9055aa9db34bceb39975e7a7af57a0d41d17e
Mnigos <makowskiigor@gmail.com>
2024-07-25 09:33:45 +0200

    style(eslint): fix errors

--- app/constants/cookies.ts
+++ app/constants/cookies.ts
@@ -1,2 +1,2 @@
-export const USER_ACCEPT_COOKIES = 'user-accept-cookies' as const
-export const USER_ID = 'user-id' as const
+export const USER_ACCEPT_COOKIES = 'user-accept-cookies'
+export const USER_ID = 'user-id'
