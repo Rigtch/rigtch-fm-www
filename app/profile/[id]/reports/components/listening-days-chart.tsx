8105bfa99305a098438c6a6069d1e46fb78151b5
Mnigos <makowskiigor@gmail.com>
2024-08-31 15:08:23 +0200

    feat(profile/reports/components/listening-days-chart): create tooltip and tick formatters

--- app/profile/[id]/reports/components/listening-days-chart.tsx
+++ app/profile/[id]/reports/components/listening-days-chart.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { BarChart, Bar, XAxis, CartesianGrid } from 'recharts'
+import prettyMilliseconds from 'pretty-ms'
+import type { ComponentProps, CSSProperties } from 'react'
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts'
 
+import { StatsMeasurement } from '@app/api/enums'
 import type { ChartConfig } from '@app/components/ui/chart'
 import {
   ChartContainer,
@@ -15,12 +18,14 @@ namespace ListeningDaysChart {
   export interface Props {
     thisWeekResponse: Record<number, number>
     lastWeekResponse: Record<number, number>
+    measurement: StatsMeasurement
   }
 }
 
 function ListeningDaysChart({
   thisWeekResponse,
   lastWeekResponse,
+  measurement,
 }: ListeningDaysChart.Props) {
   const listeningDays = [
     {
@@ -71,6 +76,47 @@ function ListeningDaysChart({
     },
   } satisfies ChartConfig
 
+  type ChartTooltipContentFormatter = ComponentProps<
+    typeof ChartTooltipContent
+  >['formatter']
+
+  const formatter: ChartTooltipContentFormatter = (value, name, item) => {
+    const formattedValue =
+      measurement === StatsMeasurement.PLAYS
+        ? `${value} plays`
+        : prettyMilliseconds(+value, { unitCount: 2 })
+
+    return (
+      <>
+        <div
+          className="shrink-0 rounded-[2px] border-[--color-border] bg-[--color-bg] h-2.5 w-2.5"
+          style={
+            {
+              '--color-bg': item.color,
+              '--color-border': item.color,
+            } as CSSProperties
+          }
+        />
+
+        <div className="flex flex-1 gap-1 justify-between leading-none items-end">
+          <div className="grid gap-1.5">
+            <span className="text-muted-foreground">{item.name}</span>
+          </div>
+          {item.value && (
+            <span className="font-mono font-medium tabular-nums text-foreground">
+              {formattedValue}
+            </span>
+          )}
+        </div>
+      </>
+    )
+  }
+
+  const tickFormatter = (value: number) =>
+    measurement === StatsMeasurement.PLAYS
+      ? value + ''
+      : prettyMilliseconds(value, { unitCount: 2 })
+
   return (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full dark">
       <BarChart accessibilityLayer data={listeningDays}>
@@ -81,8 +127,15 @@ function ListeningDaysChart({
           axisLine={false}
           tickFormatter={(value: string) => value.slice(0, 3)}
         />
+        <YAxis
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickCount={8}
+          tickFormatter={tickFormatter}
+        />
         <CartesianGrid vertical={false} />
-        <ChartTooltip content={<ChartTooltipContent />} />
+        <ChartTooltip content={<ChartTooltipContent />} formatter={formatter} />
         <ChartLegend content={<ChartLegendContent />} />
         <Bar dataKey="thisWeek" fill="var(--color-thisWeek)" radius={4} />
         <Bar dataKey="lastWeek" fill="var(--color-lastWeek)" radius={4} />
