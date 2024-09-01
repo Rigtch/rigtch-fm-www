1d0cd3a5710e199a7a675b9e50b8db5cffef082f
Mnigos <makowskiigor@gmail.com>
2024-09-01 14:41:04 +0200

    fix(components/sidebar): passing through `searchParams`

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -13,10 +13,15 @@ import {
 
 import { SidebarSectionItem } from './sidebar-section-item'
 
-import { STATS_PROVIDER, VIEW } from '@app/profile/constants'
+import {
+  STATS_MEASUREMENT,
+  STATS_PROVIDER,
+  TIME_RANGE,
+  VIEW,
+} from '@app/profile/constants'
 import { StatsProvider } from '@app/profile/enums'
+import { validateStatsProvider } from '@app/profile/utils/validators'
 import type { ParamsWithId } from '@app/types'
-import { formatSearchParams } from '@app/utils/formatters'
 
 export function Sidebar() {
   const pathname = usePathname()
@@ -24,54 +29,89 @@ export function Sidebar() {
   const { id: userId } = useParams<ParamsWithId>()
 
   const view = searchParams.get(VIEW)
+  const statsProvider = searchParams.get(STATS_PROVIDER)
+  const timeRange = searchParams.get(TIME_RANGE)
+  const statsMeasurement = searchParams.get(STATS_MEASUREMENT)
+
+  const profileSearchParams = new URLSearchParams()
+  const topGenresSearchParams = new URLSearchParams()
+  const topArtistsSearchParams = new URLSearchParams()
+  const topAlbumsSearchParams = new URLSearchParams()
+  const topTracksSearchParams = new URLSearchParams()
+  const reportsSearchParams = new URLSearchParams()
+
+  if (view) {
+    profileSearchParams.set(VIEW, view)
+    topArtistsSearchParams.set(VIEW, view)
+    topAlbumsSearchParams.set(VIEW, view)
+    topTracksSearchParams.set(VIEW, view)
+  }
+  if (statsProvider) {
+    profileSearchParams.set(STATS_PROVIDER, statsProvider)
+    topArtistsSearchParams.set(STATS_PROVIDER, statsProvider)
+    topGenresSearchParams.set(STATS_PROVIDER, statsProvider)
+    topTracksSearchParams.set(STATS_PROVIDER, statsProvider)
+  }
+  if (timeRange) {
+    profileSearchParams.set(TIME_RANGE, timeRange)
+    topArtistsSearchParams.set(TIME_RANGE, timeRange)
+    topGenresSearchParams.set(TIME_RANGE, timeRange)
+    if (validateStatsProvider(statsProvider) === StatsProvider.RIGTCH)
+      topAlbumsSearchParams.set(TIME_RANGE, timeRange)
+    topTracksSearchParams.set(TIME_RANGE, timeRange)
+  }
+  if (statsMeasurement) {
+    profileSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+    topArtistsSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+    topAlbumsSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+    topGenresSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+    topTracksSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+    reportsSearchParams.set(STATS_MEASUREMENT, statsMeasurement)
+  }
 
   return (
     <section className="flex flex-col gap-2">
       <header>
-        <h3 className="text-xl font-semibold px-2">Profile</h3>
+        <h3 className="px-2 text-xl font-semibold">Profile</h3>
       </header>
 
       <main className="flex flex-col gap-2">
         <SidebarSectionItem
-          href={`/profile/${userId}?${searchParams.toString()}`}
+          href={`/profile/${userId}?${profileSearchParams.toString()}`}
           label="Overview"
           pathname={pathname}
           icon={LuUserCircle}
         />
 
-        <section className="pl-4 flex flex-col gap-2">
+        <section className="flex flex-col gap-2 pl-4">
           <header>
-            <h4 className="text-lg font-semibold px-2">Top</h4>
+            <h4 className="px-2 text-lg font-semibold">Top</h4>
           </header>
 
           <main>
             <SidebarSectionItem
-              href={`/profile/${userId}/top/genres?${searchParams.toString()}`}
+              href={`/profile/${userId}/top/genres?${topGenresSearchParams.toString()}`}
               label="Top Genres"
               pathname={pathname}
               icon={LuListMusic}
             />
 
             <SidebarSectionItem
-              href={`/profile/${userId}/top/artists?${searchParams.toString()}`}
+              href={`/profile/${userId}/top/artists?${topArtistsSearchParams.toString()}`}
               label="Top Artists"
               pathname={pathname}
               icon={LuMic2}
             />
 
             <SidebarSectionItem
-              href={`/profile/${userId}/top/albums?${formatSearchParams(
-                view ? new URLSearchParams({ [VIEW]: view }) : searchParams,
-                STATS_PROVIDER,
-                StatsProvider.RIGTCH
-              )}`}
+              href={`/profile/${userId}/top/albums?${topAlbumsSearchParams.toString()}`}
               label="Top Albums"
               pathname={pathname}
               icon={LuDisc3}
             />
 
             <SidebarSectionItem
-              href={`/profile/${userId}/top/tracks?${searchParams.toString()}`}
+              href={`/profile/${userId}/top/tracks?${topTracksSearchParams.toString()}`}
               label="Top Tracks"
               pathname={pathname}
               icon={LuMusic}
@@ -80,7 +120,7 @@ export function Sidebar() {
         </section>
 
         <SidebarSectionItem
-          href={`/profile/${userId}/reports`}
+          href={`/profile/${userId}/reports?${reportsSearchParams.toString()}`}
           label="Reports"
           pathname={pathname}
           icon={LuBarChartBig}
