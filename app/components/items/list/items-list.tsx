5ed9faddfd72a03dab0c48c3e676a5e81126c3ea
Mnigos <makowskiigor@gmail.com>
2024-09-01 11:52:28 +0200

    refactor(components/items): create `itemsFormatter` and support `className`

--- app/components/items/list/items-list.tsx
+++ app/components/items/list/items-list.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import type { HtmlHTMLAttributes } from 'react'
+
 import { ItemTopCard } from '../cards'
 import type { ItemPosition } from '../misc'
+import { formatItems } from '../helpers'
 
 import { ItemsListElement } from './items-list-element'
 
-import type { StatsMeasurement } from '@app/api/enums'
 import type {
   AlbumEntity,
   ArtistEntity,
@@ -20,9 +22,11 @@ import {
   CarouselPrevious,
 } from '@app/components/ui/carousel'
 import { Separator } from '@app/components/ui/separator'
+import { cn } from '@app/utils/cn'
 
 namespace ItemsList {
-  export interface Props {
+  export interface Props
+    extends Pick<HtmlHTMLAttributes<HTMLDivElement>, 'className'> {
     items:
       | ArtistEntity[]
       | TrackEntity[]
@@ -31,6 +35,7 @@ namespace ItemsList {
     positionSize?: ItemPosition.Props['size']
     positionClassName?: string
     lastItemSeparator?: boolean
+    isRounded?: boolean
   }
 }
 
@@ -38,67 +43,37 @@ function ItemsList({
   items,
   isTop,
   positionSize,
+  className,
   positionClassName,
   lastItemSeparator = false,
+  isRounded = false,
 }: ItemsList.Props) {
-  const sortedItems: (ArtistEntity | TrackEntity | AlbumEntity)[] = items.map(
-    (item, index) => ({
-      ...('item' in item
-        ? 'plays' in item
-          ? {
-              ...item.item,
-              plays: item.plays,
-              maxPlays: Math.max(
-                ...(
-                  items as RigtchStatsResponse<
-                    ArtistEntity | TrackEntity | AlbumEntity,
-                    StatsMeasurement.PLAYS
-                  >
-                ).map(item => item.plays)
-              ),
-            }
-          : {
-              ...item.item,
-              playTime: item.playTime,
-              maxPlayTime: Math.max(
-                ...(
-                  items as RigtchStatsResponse<
-                    ArtistEntity | TrackEntity | AlbumEntity,
-                    StatsMeasurement.PLAY_TIME
-                  >
-                ).map(item => item.playTime)
-              ),
-            }
-        : item),
-      ...(!('playedAt' in items[0]) && {
-        position: index + 1,
-      }),
-    })
-  )
+  const sortedItems: (ArtistEntity | TrackEntity | AlbumEntity)[] =
+    formatItems(items)
 
   const carouselItems = sortedItems.slice(0, 3)
 
   if (isTop) sortedItems.splice(0, 2, sortedItems[1], sortedItems[0])
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className={cn('flex flex-col gap-8', className)}>
       {isTop && (
         <>
-          <div className="hidden md:flex flex-col md:flex-row self-center items-center md:items-start justify-center gap-4 pt-4 mt-16 lg:mt-24 w-full">
-            <div className="flex flex-col-reverse md:flex-row justify-center gap-4 md:w-2/3 h-full">
+          <div className="mt-16 hidden w-full flex-col items-center justify-center gap-4 self-center pt-4 md:flex md:flex-row md:items-start lg:mt-24">
+            <div className="flex h-full flex-col-reverse justify-center gap-4 md:w-2/3 md:flex-row">
               {sortedItems.slice(0, 2).map(item => (
                 <ItemTopCard {...item} key={item.id} />
               ))}
             </div>
 
-            <div className="md:w-1/3 h-full">
+            <div className="h-full md:w-1/3">
               {sortedItems.slice(2, 3).map(item => (
                 <ItemTopCard {...item} key={item.id} />
               ))}
             </div>
           </div>
 
-          <Carousel className="md:hidden self-center max-w-[60vw] sm:max-w-[300px]">
+          <Carousel className="max-w-[60vw] self-center sm:max-w-[300px] md:hidden">
             <CarouselContent>
               {carouselItems.slice(0, 3).map(item => (
                 <CarouselItem key={item.id}>
@@ -120,6 +95,10 @@ function ItemsList({
               {...item}
               positionSize={positionSize}
               positionClassName={positionClassName}
+              className={cn(
+                isRounded && index === 0 && 'rounded-t-lg',
+                isRounded && index === items.length - 1 && 'rounded-bl-lg'
+              )}
             />
 
             {items.length === index + 1 ? (
