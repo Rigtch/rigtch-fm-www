3670ed59bafd43192f7e38c345b1040f20de0b1e
Mnigos <makowskiigor@gmail.com>
2024-09-03 13:27:52 +0200

    test(profile/reports): create missing unit tests

---
+++ app/profile/[id]/reports/components/cards/stat-card.spec.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react'
+
+import { StatCard } from './stat-card'
+
+describe('StatCard', () => {
+  test('should match snapshot', () => {
+    const view = render(<StatCard label="label">100</StatCard>)
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot with valueSize lg', () => {
+    const view = render(
+      <StatCard label="label" valueSize="lg">
+        100
+      </StatCard>
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot with valueSize xl', () => {
+    const view = render(
+      <StatCard label="label" valueSize="xl">
+        100
+      </StatCard>
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+
+  test('should match snapshot with vs last week percent', () => {
+    const view = render(
+      <StatCard label="label" valueSize="xl" value={100} lastWeekValue={50}>
+        100
+      </StatCard>
+    )
+
+    expect(view).toMatchSnapshot()
+  })
+})
