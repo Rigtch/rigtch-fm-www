70387a93e21efa51a53a68a869095db4e234d08b
Mnigos <makowskiigor@gmail.com>
2024-09-07 13:31:16 +0200

    fix(components/items/cards/item-top/skeleton): stars positioning

--- app/components/items/cards/item-top-card.skeleton.tsx
+++ app/components/items/cards/item-top-card.skeleton.tsx
@@ -27,36 +27,36 @@ function ItemTopCardSkeleton({
   return (
     <div
       className={cn(
-        'w-full flex flex-col justify-start gap-2 h-full relative',
+        'relative flex h-full w-full flex-col justify-start gap-2',
         position === 1 && '-top-16 lg:-top-24',
         position === 2 && '-top-8 lg:-top-12'
       )}
     >
-      <header className="w-full flex flex-col gap-2 items-center p-0">
-        <div className="p-1 rounded-xl bg-card">
-          <ItemImageSkeleton className="w-[164px] h-[164px]" />
+      <header className="flex w-full flex-col items-center gap-2 p-0">
+        <div className="rounded-xl bg-card p-1">
+          <ItemImageSkeleton className="h-[164px] w-[164px]" />
         </div>
 
         <div className="flex flex-col items-center gap-1">
-          <Skeleton className="w-[12rem] h-[30px]" />
+          <Skeleton className="h-[30px] w-[12rem]" />
 
-          {withArtists && <Skeleton className="w-[6rem] h-[25px]" />}
+          {withArtists && <Skeleton className="h-[25px] w-[6rem]" />}
         </div>
 
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col items-center justify-center gap-4">
           <span className="text-center text-5xl">{position}</span>
 
           {withGenres && (
-            <div className="flex flex-row gap-2 flex-wrap justify-center h-full max-w-full">
-              <Skeleton className="w-[4rem] h-[1rem] rounded-pill" />
-              <Skeleton className="w-[6rem] h-[1rem] rounded-pill" />
-              <Skeleton className="w-[5rem] h-[1rem] rounded-pill" />
+            <div className="flex h-full max-w-full flex-row flex-wrap justify-center gap-2">
+              <Skeleton className="rounded-pill h-[1rem] w-[4rem]" />
+              <Skeleton className="rounded-pill h-[1rem] w-[6rem]" />
+              <Skeleton className="rounded-pill h-[1rem] w-[5rem]" />
             </div>
           )}
 
-          <div className="flex flex-row gap-1 mt-4 w-full">
+          <div className="mt-4 flex w-full flex-row justify-center gap-1">
             {withProgress ? (
-              <Skeleton className="w-full h-7 rounded-full" />
+              <Skeleton className="h-7 w-full rounded-full" />
             ) : (
               stars.map((size, index) => (
                 <FaStar
