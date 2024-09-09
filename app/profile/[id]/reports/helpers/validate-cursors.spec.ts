b69b54376fc4cc1664fbf17e6e1b58732bfbfffb
Mnigos <makowskiigor@gmail.com>
2024-09-09 10:11:40 +0200

    test(profile/reports/helpers/validate-cursors): make tests more valuable

--- app/profile/[id]/reports/helpers/validate-cursors.spec.ts
+++ app/profile/[id]/reports/helpers/validate-cursors.spec.ts
@@ -4,25 +4,15 @@ describe('validateCursors', () => {
   test('should return default cursors', () => {
     const { before, after } = validateCursors()
 
-    expect(before.getFullYear()).toBe(new Date().getFullYear())
-    expect(before.getMonth()).toBe(new Date().getMonth())
-    expect(before.getDate()).toEqual(expect.any(Number))
-    expect(after.getFullYear()).toBe(new Date().getFullYear())
-    expect(after.getMonth()).toBe(new Date().getMonth() - 1)
-    expect(after.getDate()).toEqual(expect.any(Number))
+    expect(before.getTime() - after.getTime()).toBe(1000 * 60 * 60 * 24 * 7)
   })
 
   test('should return custom cursors', () => {
     const { before, after } = validateCursors(
-      '2022-01-01T00:00:00.000Z',
-      '2022-01-02T00:00:00.000Z'
+      '2022-01-08T00:00:00.000Z',
+      '2022-01-01T00:00:00.000Z'
     )
 
-    expect(before.getFullYear()).toBe(2022)
-    expect(before.getMonth()).toBe(0)
-    expect(before.getDate()).toBe(1)
-    expect(after.getFullYear()).toBe(2022)
-    expect(after.getMonth()).toBe(0)
-    expect(after.getDate()).toBe(2)
+    expect(before.getTime() - after.getTime()).toBe(1000 * 60 * 60 * 24 * 7)
   })
 })
