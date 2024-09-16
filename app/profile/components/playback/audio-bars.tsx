450003bd1e4a2c2c613427af23878c7ca4f47b0a
Mnigos <makowskiigor@gmail.com>
2024-09-17 00:00:27 +0200

    refactor(profile/components/playback/audio-bars): remove `isPlaying` prop

--- app/profile/components/playback/audio-bars.tsx
+++ app/profile/components/playback/audio-bars.tsx
@@ -1,27 +1,17 @@
 import { cn } from '@app/utils/cn'
 
-namespace AudioBars {
-  export type Props = Readonly<{
-    isPlaying: boolean
-  }>
-}
-
-function AudioBars({ isPlaying = false }: AudioBars.Props) {
+function AudioBars() {
   return (
     <div className={cn('relative h-[28px] w-[28px] overflow-hidden')}>
       {Array.from({ length: 4 }).map((_, index) => (
         <span
           key={index}
           className={cn(
-            'absolute h-[28px] w-[4px] bg-white transition-all ease-linear',
+            'absolute h-[28px] w-[4px] animate-up-down bg-white transition-all ease-linear',
             index === 0 && 'left-0',
-            index === 1 && 'left-[8px]',
-            index === 1 && isPlaying && 'animation-delay-[0.6s]',
-            index === 2 && 'left-[16px]',
-            index === 2 && isPlaying && 'animation-delay-[0.3s]',
-            index === 3 && 'left-[24px]',
-            index === 3 && isPlaying && 'animation-delay-[0.9s]',
-            isPlaying && 'animate-up-down'
+            index === 1 && 'left-[8px] animation-delay-[0.6s]',
+            index === 2 && 'left-[16px] animation-delay-[0.3s]',
+            index === 3 && 'left-[24px] animation-delay-[0.9s]'
           )}
         />
       ))}
