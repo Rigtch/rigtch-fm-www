881850d5b320f4ae73a0e4d33685c0facb5f534b
Mnigos <makowskiigor@gmail.com>
2024-09-19 13:43:00 +0200

    refactor(profile/reports/components/charts): move `colors` to separate file

---
+++ app/profile/[id]/reports/helpers/chart-colors.ts
@@ -0,0 +1,9 @@
+export const chartColors = [
+  '#9400d5',
+  '#590080',
+  '#d500ac',
+  '#2e00ef',
+  '#1e89ee',
+  '#0f6cc5',
+  '#0b5090',
+]
--- app/profile/[id]/reports/helpers/index.ts
+++ app/profile/[id]/reports/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './week-days'
 export * from './value-measurement-formatter'
 export * from './validate-cursors'
+export * from './chart-colors'
