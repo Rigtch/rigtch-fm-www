be72955cd64239a54bb70e82115bf1aefeb3d5aa
Mnigos <makowskiigor@gmail.com>
2024-09-23 10:34:57 +0200

    refactor(profile/reports/page): add all props to a reusable variable

--- app/profile/[id]/reports/page.tsx
+++ app/profile/[id]/reports/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react'
 import type { ProfileReportsPageProps } from './types/props'
 import { validateCursors } from './helpers'
 import { ListeningDaysView, ListeningHoursView } from './views'
+import type { ReportsViewProps } from './views/types/props'
 
 import { validateStatsMeasurement } from '@app/profile/utils/validators'
 import { STATS_MEASUREMENT } from '@app/profile/constants'
@@ -25,24 +26,21 @@ export default async function ProfileReportsPage({
   const measurement = validateStatsMeasurement(searchParams[STATS_MEASUREMENT])
   const cursors = validateCursors(searchParams.before, searchParams.after)
 
+  const viewProps: ReportsViewProps = {
+    token: token ?? '',
+    userId,
+    measurement,
+    cursors,
+  }
+
   return (
     <>
       <Suspense>
-        <ListeningDaysView
-          token={token ?? ''}
-          userId={userId}
-          measurement={measurement}
-          cursors={cursors}
-        />
+        <ListeningDaysView {...viewProps} />
       </Suspense>
 
       <Suspense>
-        <ListeningHoursView
-          token={token ?? ''}
-          userId={userId}
-          measurement={measurement}
-          cursors={cursors}
-        />
+        <ListeningHoursView {...viewProps} />
       </Suspense>
     </>
   )
