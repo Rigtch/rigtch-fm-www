7b99a8ff08c3574ab60ebd17199f06a1a1fc5b07
Mnigos <makowskiigor@gmail.com>
2024-10-07 17:50:40 +0200

    feat(api/types): create user following and followers types

---
+++ app/api/types/user-following.ts
@@ -0,0 +1,9 @@
+import type { Profile } from './profile'
+import type { User } from './user'
+
+export interface UserFollowing extends Pick<User, 'id'> {
+  following: Pick<User, 'id'> &
+    {
+      profile: Pick<Profile, 'displayName' | 'images' | 'href'>
+    }[]
+}
