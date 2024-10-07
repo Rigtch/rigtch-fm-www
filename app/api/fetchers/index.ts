03dbd58b47da6bccab54a1d6394f909cc890fc25
Mnigos <makowskiigor@gmail.com>
2024-10-07 17:49:11 +0200

    feat(api/fetchers): create follow and unfollow methods

--- app/api/fetchers/index.ts
+++ app/api/fetchers/index.ts
@@ -4,8 +4,11 @@ export * from './get-history'
 export * from './get-playback-state'
 export * from './get-recently-played'
 export * from './get-user'
+export * from './get-user-followers'
 
 export * from './post-me-user'
 
+export * from './put-follow-user'
+export * from './put-unfollow-user'
 export * from './put-player-pause'
 export * from './put-player-resume'
