2de6c5a8ccede65480dcfb0ee8c7d4a52353d34d
Mnigos <makowskiigor@gmail.com>
2024-10-07 17:51:44 +0200

    feat(profile): create follow/unfollow button

--- app/profile/[id]/@profile/page.tsx
+++ app/profile/[id]/@profile/page.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next'
+import { cookies } from 'next/headers'
 
-import { getUser } from '@app/api/fetchers'
+import { getUser, getUserFollowers } from '@app/api/fetchers'
 import { getServerToken } from '@app/auth/utils'
 import { Playback } from '@app/profile/components/playback'
 import { ProfileCard } from '@app/profile/components/profile'
 import type { ProfilePageProps } from '@app/profile/types'
 import { validateId } from '@app/utils/validators'
+import { USER_ID } from '@app/constants'
 
 export const runtime = 'edge'
 
@@ -27,13 +29,23 @@ export async function generateMetadata({
 export default async function ProfileSubPage({ params }: ProfilePageProps) {
   const userId = validateId(params.id)
   const token = await getServerToken(userId)
-  const { profile, followersCount, followingCount } = await getUser(token, {
-    userId,
-  })
+
+  const [{ profile, followersCount, followingCount, id }, { followers }] =
+    await Promise.all([
+      getUser(token, {
+        userId,
+      }),
+      getUserFollowers(token, { userId }),
+    ])
+  const currentUserId = validateId(cookies().get(USER_ID)?.value)
+  const isFollowingUser = followers.some(({ id }) => id === currentUserId)
 
   return (
     <ProfileCard
       {...profile}
+      id={id}
+      isFollowingUser={isFollowingUser}
+      currentUserId={currentUserId}
       followersCount={followersCount}
       followingCount={followingCount}
     >
--- app/profile/components/profile/profile-card.tsx
+++ app/profile/components/profile/profile-card.tsx
@@ -1,17 +1,24 @@
-import type { HTMLAttributes } from 'react'
+'use client'
+
+import { useState, type HTMLAttributes } from 'react'
 
 import { ProfileAvatar } from './profile-avatar'
 import { ShareButton } from './share-button'
 
 import type { Profile, User } from '@app/api/types'
 import { SpotifyLink } from '@app/components/common'
-import { CardDescription, CardTitle } from '@app/components/ui/card'
+import { Button } from '@app/components/ui/button'
 import { findImage } from '@app/utils/find-image'
+import { useToggleFollowing } from '@app/api/hooks'
+import { cn } from '@app/utils/cn'
 
 namespace ProfileCard {
   export type Props = Pick<Profile, 'displayName' | 'images' | 'href'> &
-    Pick<User, 'followersCount' | 'followingCount'> &
-    Pick<HTMLAttributes<HTMLDivElement>, 'children'>
+    Pick<User, 'followersCount' | 'followingCount' | 'id'> &
+    Pick<HTMLAttributes<HTMLDivElement>, 'children'> & {
+      currentUserId: string
+      isFollowingUser: boolean
+    }
 }
 
 function ProfileCard({
@@ -19,9 +26,22 @@ function ProfileCard({
   images,
   followersCount,
   followingCount,
+  id,
+  currentUserId,
+  isFollowingUser,
   href,
   children,
 }: ProfileCard.Props) {
+  const [isFollowingUserOptimistic, setIsFollowingUserOptimistic] =
+    useState(isFollowingUser)
+  const { toggle } = useToggleFollowing()
+
+  async function handleToggleFollowing() {
+    setIsFollowingUserOptimistic(isFollowing => !isFollowing)
+
+    await toggle(isFollowingUserOptimistic)
+  }
+
   return (
     <div className="min-w-[75%]">
       <div className="flex flex-col justify-between gap-6 md:flex-row md:items-center">
@@ -34,18 +54,35 @@ function ProfileCard({
 
           <div className="flex h-full flex-col justify-between gap-4">
             <div>
-              <CardTitle className="text-2xl md:text-3xl">
-                {displayName}
-              </CardTitle>
+              <div className="text-2xl md:text-3xl">{displayName}</div>
 
-              <CardDescription className="flex items-center gap-2 whitespace-nowrap text-lg leading-5 text-foreground">
+              <div className="flex items-center gap-2 whitespace-nowrap text-lg leading-5 text-foreground">
                 <SpotifyLink href={href} />
                 <p>{followersCount} Followers</p>&bull;
                 <p>{followingCount} Following</p>
-              </CardDescription>
+              </div>
             </div>
 
             <div className="items-left flex flex-row gap-2">
+              <Button
+                variant="outline"
+                className={cn(
+                  'group hover:bg-white',
+                  isFollowingUserOptimistic &&
+                    "hover:before:content-['Unfollow']"
+                )}
+                disabled={id === currentUserId}
+                onClick={handleToggleFollowing}
+              >
+                <span
+                  className={cn(
+                    isFollowingUserOptimistic && 'group-hover:hidden'
+                  )}
+                >
+                  {isFollowingUserOptimistic ? 'Following' : 'Follow'}
+                </span>
+              </Button>
+
               <ShareButton />
             </div>
           </div>
