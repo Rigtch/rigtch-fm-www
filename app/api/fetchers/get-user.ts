7ec7fed8751ee32801725acd6826e57f8c708d9b
Mnigos <makowskiigor@gmail.com>
2024-10-07 17:47:16 +0200

    feat(api/fetchers/get-user): add `user` tag

--- app/api/fetchers/get-user.ts
+++ app/api/fetchers/get-user.ts
@@ -5,6 +5,9 @@ import { fetchApi } from './fetch-api'
 export function getUser(token: string, { userId }: UsersParams) {
   return fetchApi<User>(`/users/${userId}`, {
     token,
+    next: {
+      tags: ['user'],
+    },
   }).then(({ createdAt, ...user }) => ({
     ...user,
     ...(createdAt && { createdAt: new Date(createdAt) }),
