ec8aff81b8a1e464c6b254d0b491002afd1971e9
Mnigos <makowskiigor@gmail.com>
2024-10-12 14:19:31 +0200

    feat(profile/sections): add `TotalItemsListened` component

--- app/profile/sections/index.ts
+++ app/profile/sections/index.ts
@@ -1,6 +1,7 @@
 export * from './items-section'
 export * from './history-section'
 export * from './top-genres-section'
+export * from './total-items-listened'
 
 export * from './items-section.skeleton'
 export * from './top-genres-section.skeleton'
