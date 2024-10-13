6fe7355217a5d1584c3c5de8833e0241fcd990ee
Mnigos <makowskiigor@gmail.com>
2024-10-13 12:36:46 +0200

    fix(profile/sections/total-items-listened): format total value

--- app/profile/sections/total-items-listened.tsx
+++ app/profile/sections/total-items-listened.tsx
@@ -4,7 +4,7 @@ import type { ItemType } from '@app/api/types'
 
 namespace TotalItemsListened {
   export type Props = Readonly<
-    Pick<ItemsSection.Props, 'total'> & {
+    Required<Pick<ItemsSection.Props, 'total'>> & {
       itemType: ItemType
     }
   >
@@ -13,7 +13,9 @@ namespace TotalItemsListened {
 function TotalItemsListened({ total, itemType }: TotalItemsListened.Props) {
   return (
     <div className="flex h-full flex-wrap items-center gap-2">
-      <span className="text-3xl font-semibold leading-5">{total} </span>
+      <span className="text-3xl font-semibold leading-5">
+        {new Intl.NumberFormat('en-EN').format(total)}
+      </span>
       <span className="text-xl">{itemType}s listened</span>
     </div>
   )
