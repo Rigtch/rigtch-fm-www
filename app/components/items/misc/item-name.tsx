86bce31998d4a9fd4dfa1a32417d102f6a644c5b
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-11-25 21:44:37 +0100

    feat(item-name): make name bigger in carousel

--- app/components/items/misc/item-name.tsx
+++ app/components/items/misc/item-name.tsx
@@ -13,14 +13,7 @@ namespace ItemName {
 function ItemName({ name, href, className }: ItemName.Props) {
   return (
     <LinkButton href={href} className="flex justify-start">
-      <p
-        className={cn(
-          'max-w-[350px] truncate sm:max-w-[400px] md:max-w-[240px] xl:max-w-[340px]',
-          className
-        )}
-      >
-        {name}
-      </p>
+      <p className={cn('max-w-[400px] truncate', className)}>{name}</p>
     </LinkButton>
   )
 }
