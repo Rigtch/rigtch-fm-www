e6ec5823fd8d779bddceebdebe6059dff36f9442
Mnigos <makowskiigor@gmail.com>
2024-11-28 16:29:40 +0100

    feat(app/components/ui): add `ShineBorder`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,11 +87,23 @@ export default {
           '90%': { transform: 'translateY(20px)' },
           '100%': { transform: 'translateY(0)' },
         },
+        shine: {
+          '0%': {
+            'background-position': '0% 0%',
+          },
+          '50%': {
+            'background-position': '100% 100%',
+          },
+          to: {
+            'background-position': '0% 0%',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'up-down': 'up-down 2.5s linear infinite',
+        shine: 'shine var(--duration) infinite linear',
       },
     },
   },
